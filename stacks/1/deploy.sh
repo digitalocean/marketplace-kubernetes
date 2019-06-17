b4f091345c41c0703d376448252e2b4337e4e91d
Dhruv Kela <dkela@Dhruv-Kela-MBP-15.local>
2019-06-17 16:26:12 -0400

    Added deploy.sh

--- stacks/1/deploy.sh
+++ stacks/1/deploy.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+
+set -e 
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+#creating prometheus namespace
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: prometheus
+EOF
+
+#creating grafana namespace
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: grafana
+EOF
+
+#spinning up the solution stack
+#prometheus
+kubectl apply -f "$ROOT_DIR"/stacks/1/yaml/prometheus.yaml
+#grafana
+kubectl apply -f "$ROOT_DIR"/stacks/1/yaml/grafana.yaml
