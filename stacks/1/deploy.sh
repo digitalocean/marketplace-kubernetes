fb2260bfda3acb6afad10d80df19ff404e814247
Paul Anunda <paulanunda@gmail.com>
2019-06-18 09:53:16 -0500

    polished deploy.sh

--- stacks/1/deploy.sh
+++ stacks/1/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -e 
+set -e
 
-#creating prometheus namespace
+# create prometheus namespace
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Namespace
@@ -10,7 +10,7 @@ metadata:
   name: prometheus
 EOF
 
-#creating grafana namespace
+# create grafana namespace
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Namespace
@@ -18,8 +18,10 @@ metadata:
   name: grafana
 EOF
 
-#spinning up the solution stack
-#prometheus
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/package_prometheus_grafana/stacks/1/yaml/prometheus.yaml?token=AHIGBPHKOYBL6EDHYW3GMRS5CIP6W -n prometheus
-#grafana
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/package_prometheus_grafana/stacks/1/yaml/grafana.yaml?token=AHIGBPA77LSWUNDJ4XR3ECS5CIP36 -n grafana
+# deploy prometheus
+kubectl -n prometheus apply -f \
+  https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/package_prometheus_grafana/stacks/1/yaml/prometheus.yaml?token=AHIGBPHKOYBL6EDHYW3GMRS5CIP6W
+
+# deploy grafana
+kubectl  -n grafana apply -f \
+  https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/package_prometheus_grafana/stacks/1/yaml/grafana.yaml?token=AHIGBPA77LSWUNDJ4XR3ECS5CIP36
