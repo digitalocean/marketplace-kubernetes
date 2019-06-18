d42070985cbc501c3bed746af1987ff05466ba65
Paul Anunda <paulanunda@gmail.com>
2019-06-18 17:11:11 -0500

    updated render.sh for stack 1

--- stacks/1/render.sh
+++ stacks/1/render.sh
@@ -5,34 +5,23 @@ set -e
 BUILD_DIR=$(mktemp -d)
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-GRAFANA_CHART_VERSION="3.4.2"
-PROMETHEUS_CHART_VERSION="8.11.4"
+PROMETHEUS_OPERATOR_CHART_VERSION="5.12.3"
 
-cp -r "$ROOT_DIR"/packages/grafana/charts/"$GRAFANA_CHART_VERSION" $BUILD_DIR
-cp -r "$ROOT_DIR"/packages/prometheus/charts/"$PROMETHEUS_CHART_VERSION" $BUILD_DIR
+cp -r "$ROOT_DIR"/charts/prometheus-operator/"$PROMETHEUS_OPERATOR_CHART_VERSION" $BUILD_DIR
 cp -r "$ROOT_DIR"/stacks/1 $BUILD_DIR
 
 cd $BUILD_DIR
 
-# Remove grafana test templates
-rm -rf "$GRAFANA_CHART_VERSION"/templates/tests
+# Remove test templates
+find "$PROMETHEUS_OPERATOR_CHART_VERSION" -type d -name tests -print0 | xargs -0 rm -rf
 
 # Create YAML directory
+rm -rf "$ROOT_DIR"/stacks/1/yaml
 mkdir -p "$ROOT_DIR"/stacks/1/yaml
 
-# render grafana
+# render prometheus-operator
 helm template \
-  --name grafana \
-  --namespace grafana \
-  --values 1/values/grafana.yaml \
-  "$GRAFANA_CHART_VERSION" > "$ROOT_DIR"/stacks/1/yaml/grafana.yaml
-
-# render prometheus
-helm template \
-  --name prometheus \
-  --namespace prometheus \
-  --values 1/values/prometheus.yaml \
-  "$PROMETHEUS_CHART_VERSION" > "$ROOT_DIR"/stacks/1/yaml/prometheus.yaml
-
-# render kuberentes-mixin dashboards
-kubectl kustomize 1/kubernetes-mixin-dashboards > "$ROOT_DIR"/stacks/1/yaml/k8s-mixin-dashboards.yaml
+  --name prometheus-operator \
+  --namespace prometheus-operator \
+  --values 1/values/prometheus-operator.yaml \
+  "$PROMETHEUS_OPERATOR_CHART_VERSION" > "$ROOT_DIR"/stacks/1/yaml/prometheus-operator.yaml
