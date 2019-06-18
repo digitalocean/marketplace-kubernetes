bb845fbd54f22bc80886b740dd84e016d179e4e7
Paul Anunda <paulanunda@gmail.com>
2019-06-18 10:11:32 -0500

    deploy grafana dashboards

--- stacks/1/deploy.sh
+++ stacks/1/deploy.sh
@@ -22,6 +22,10 @@ EOF
 kubectl -n prometheus apply -f \
   https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/package_prometheus_grafana/stacks/1/yaml/prometheus.yaml?token=AHIGBPHKOYBL6EDHYW3GMRS5CIP6W
 
+# deploy grafana dashboards
+kubectl -n grafana apply -f \
+  https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/package_prometheus_grafana/stacks/1/yaml/k8s-mixin-dashboards.yaml
+
 # deploy grafana
 kubectl -n grafana apply -f \
   https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/package_prometheus_grafana/stacks/1/yaml/grafana.yaml?token=AHIGBPA77LSWUNDJ4XR3ECS5CIP36
