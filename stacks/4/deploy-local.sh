5e9fc772bd7259889532f984398144c573918043
Dhruv Kela <dkela@Dhruv-Kela-MBP-15.local>
2019-06-21 16:52:27 -0400

    Added nginx-ingress

---
+++ stacks/4/deploy-local.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+# create nginx-ingress namespace
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: nginx-ingress
+EOF
+
+# set kubectl namespace
+kubectl config set-context --current --namespace=nginx-ingress
+
+# deploy nginx-ingress
+kubectl apply -f "$ROOT_DIR"/stacks/4/yaml/nginx-ingress.yaml
+
+# ensure services are running
+kubectl rollout status deployment/nginx-ingress-default-backend
+kubectl rollout status deployment/nginx-ingress-controller
