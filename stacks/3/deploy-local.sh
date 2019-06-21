efd26950c0e7305c3addfa44de881b470f66debf
Paul Anunda <paulanunda@gmail.com>
2019-06-21 09:46:09 -0500

    render metrics-server yaml in single file

--- stacks/3/deploy-local.sh
+++ stacks/3/deploy-local.sh
@@ -4,8 +4,11 @@ set -e
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
+# set kubectl namespace
+kubectl config set-context --current --namespace=kube-system
+
 # deploy metrics-server
-kubectl apply -f "$ROOT_DIR"/stacks/3/yaml/.
+kubectl apply -f "$ROOT_DIR"/stacks/3/yaml/metrics-server.yaml
 
 # ensure services are running
-kubectl rollout status deployment/metrics-server -n kube-system
+kubectl rollout status deployment/metrics-server
--- stacks/3/deploy.sh
+++ stacks/3/deploy.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-# deploy prometheus-operator
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/aggregated-metrics-reader.yaml
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/auth-delegator.yaml
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/auth-reader.yaml
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/metrics-apiservice.yaml
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/metrics-server-deployment.yaml
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/metrics-server-service.yaml
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/resource-reader.yaml
+# set kubectl namespace
+kubectl config set-context --current --namespace=kube-system
+
+# deploy metrics-server
+kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/3/yaml/metrics-server.yaml
 
 # ensure services are running
-kubectl rollout status deployment/metrics-server -n kube-system
+kubectl rollout status deployment/metrics-server
