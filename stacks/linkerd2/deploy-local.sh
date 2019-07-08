22ba8835bb2ff8ca7a65537af9d932b07836ff78
Dhruv Kela <30433468+keladhruv@users.noreply.github.com>
2019-07-08 16:53:18 -0400

    Added linkerd2 (#9)

---
+++ stacks/linkerd2/deploy-local.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+LINKERD2_VERSION="stable-2.3.2"
+
+# check OS type
+if [ "$(uname -s)" = "Darwin" ]; then
+  OS=darwin
+else
+  OS=linux
+fi
+
+FILENAME="linkerd2-cli-${LINKERD2_VERSION}-${OS}"
+URL="https://github.com/linkerd/linkerd2/releases/download/${LINKERD2_VERSION}/${FILENAME}"
+BINARY="$ROOT_DIR"/stacks/linkerd2/bin/"${FILENAME}"
+
+if [ ! -f "${BINARY}" ]; then
+  # download linkerd
+  mkdir -p "$ROOT_DIR"/stacks/linkerd2/bin
+  wget ${URL} -O ${BINARY} && chmod +x ${BINARY}
+fi
+
+# set kubectl namespace
+kubectl config set-context --current --namespace=linkerd
+
+# deploy linkerd
+${BINARY} install --ignore-cluster | kubectl apply -f -
+
+# ensure services are running
+kubectl rollout status deployment/linkerd-controller
+kubectl rollout status deployment/linkerd-grafana
+kubectl rollout status deployment/linkerd-identity
+kubectl rollout status deployment/linkerd-prometheus
+kubectl rollout status deployment/linkerd-sp-validator
+kubectl rollout status deployment/linkerd-web
---
+++ stacks/linkerd2/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# set kubectl namespace
+kubectl config set-context --current --namespace=linkerd
+
+# deploy linkerd yaml
+linkerd install --ignore-cluster | kubectl apply -f -
+
+# ensure services are running
+kubectl rollout status deployment/linkerd-controller
+kubectl rollout status deployment/linkerd-grafana
+kubectl rollout status deployment/linkerd-identity
+kubectl rollout status deployment/linkerd-prometheus
+kubectl rollout status deployment/linkerd-sp-validator
+kubectl rollout status deployment/linkerd-web
