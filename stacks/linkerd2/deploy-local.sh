e6ae8295c13ea726e62719bbfceb6f35e559f869
Paul Anunda <paulanunda@gmail.com>
2019-07-08 14:36:20 -0500

    linkerd2 script updates

--- stacks/linkerd2/deploy-local.sh
+++ stacks/linkerd2/deploy-local.sh
@@ -2,28 +2,33 @@
 
 set -e
 
-#set variables
 ROOT_DIR=$(git rev-parse --show-toplevel)
-LINKERD2_VERSION=${LINKERD2_VERSION:-stable-2.3.2}
+LINKERD2_VERSION="stable-2.3.2"
 
-#check OS type
+# check OS type
 if [ "$(uname -s)" = "Darwin" ]; then
   OS=darwin
 else
   OS=linux
 fi
 
-#download linkerd
-filename="linkerd2-cli-${LINKERD2_VERSION}-${OS}"
-url="https://github.com/linkerd/linkerd2/releases/download/${LINKERD2_VERSION}/${filename}"
-curl -LO "${url}"
-chmod +x ${filename}
+FILENAME="linkerd2-cli-${LINKERD2_VERSION}-${OS}"
+URL="https://github.com/linkerd/linkerd2/releases/download/${LINKERD2_VERSION}/${FILENAME}"
+BINARY="$ROOT_DIR"/stacks/linkerd2/bin/"${FILENAME}"
+
+if [ ! -f "${BINARY}" ]; then
+  # download linkerd
+  mkdir -p "$ROOT_DIR"/stacks/linkerd2/bin
+  wget ${URL} -O ${BINARY} && chmod +x ${BINARY}
+fi
 
 # deploy linkerd
-./${filename} install | kubectl apply -f -
+${BINARY} install --ignore-cluster | kubectl apply -f -
 
 # ensure services are running
-kubectl rollout status deployment/linkerd-web
-kubectl rollout status deployment/linkerd-grafana
-kubectl rollout status deployment/linkerd-prometheus
-
+kubectl -n linkerd rollout status deployment/linkerd-controller
+kubectl -n linkerd rollout status deployment/linkerd-grafana
+kubectl -n linkerd rollout status deployment/linkerd-identity
+kubectl -n linkerd rollout status deployment/linkerd-prometheus
+kubectl -n linkerd rollout status deployment/linkerd-sp-validator
+kubectl -n linkerd rollout status deployment/linkerd-web
--- stacks/linkerd2/deploy.sh
+++ stacks/linkerd2/deploy.sh
@@ -3,7 +3,12 @@
 set -e
 
 # deploy linkerd yaml
-linkerd install | kubectl apply -f -
+linkerd install --ignore-cluster | kubectl apply -f -
 
 # ensure services are running
-kubectl rollout status deployment/linkerd
+kubectl -n linkerd rollout status deployment/linkerd-controller
+kubectl -n linkerd rollout status deployment/linkerd-grafana
+kubectl -n linkerd rollout status deployment/linkerd-identity
+kubectl -n linkerd rollout status deployment/linkerd-prometheus
+kubectl -n linkerd rollout status deployment/linkerd-sp-validator
+kubectl -n linkerd rollout status deployment/linkerd-web
