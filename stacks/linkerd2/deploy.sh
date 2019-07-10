80ca65d578bb810aad2fd4d644f4bc647a72a9d1
Paul Anunda <paulanunda@gmail.com>
2019-07-10 14:32:50 -0500

    added additional linkerd rollout monitors

--- stacks/linkerd2/deploy.sh
+++ stacks/linkerd2/deploy.sh
@@ -13,5 +13,7 @@ kubectl rollout status deployment/linkerd-controller
 kubectl rollout status deployment/linkerd-grafana
 kubectl rollout status deployment/linkerd-identity
 kubectl rollout status deployment/linkerd-prometheus
+kubectl rollout status deployment/linkerd-proxy-injector
 kubectl rollout status deployment/linkerd-sp-validator
+kubectl rollout status deployment/linkerd-tap
 kubectl rollout status deployment/linkerd-web
