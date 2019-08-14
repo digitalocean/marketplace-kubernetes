d4f5901ea7c7a17ae3f5d8c39bf7bf46941e3bfe
ChandanSagar <chandan.pradhan@mayadata.io>
2019-08-14 20:27:35 +0530

    Added stacks for OpenEBS 1.1.0
    Signed-off-by: ChandanSagar <chandan.pradhan@mayadata.io>

---
+++ stacks/openebs/deploy-local.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+# create prometheus-operator namespace
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openebs 
+EOF
+
+# set kubectl namespace
+kubectl config set-context --current --namespace=openebs
+
+# deploy openebs
+kubectl apply -f "$ROOT_DIR"/stacks/openebs/yaml/openebs.yaml
+
+# ensure services are running
+kubectl get deployments -o custom-columns=NAME:.metadata.name | tail -n +2 | while read -r line
+do
+  kubectl rollout status deployment/"$line"
+done
---
+++ stacks/openebs/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+# create namespace
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: openebs
+EOF
+
+# set kubectl namespace
+kubectl config set-context --current --namespace=openebs
+
+# deploy openebs
+kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/openebs/yaml/openebs.yaml
+
+# ensure services are running
+kubectl get deployments -o custom-columns=NAME:.metadata.name | tail -n +2 | while read -r line
+do
+  kubectl rollout status deployment/"$line"
+done
