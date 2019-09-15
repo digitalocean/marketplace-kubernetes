7e054a2706927ec39f92a0f11824f55b7bbbf81d
Alexander Andryashin <aandryashin@gmail.com>
2019-09-15 15:04:06 +0300

    Added stacks for Moon 1.3.8

---
+++ stacks/moon/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+# create moon namespace
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: moon
+EOF
+
+# set kubectl namespace
+kubectl config set-context --current --namespace=moon
+
+# deploy moon
+kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/moon/yaml/moon.yaml
+
+# ensure services are running
+kubectl rollout status deployment/moon
