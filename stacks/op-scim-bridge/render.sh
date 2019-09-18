7fe632fed209830faffbe70a99d1dd6395f1a681
cohix <connor@agilebits.com>
2019-09-18 10:47:58 -0400

    added op-scim-bridge

---
+++ stacks/op-scim-bridge/render.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+BUILD_DIR=$(mktemp -d)
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+SRC_VERSION="1.0.0"
+
+cp -r "$ROOT_DIR"/src/op-scim-bridge/"$SRC_VERSION" $BUILD_DIR
+cp -r "$ROOT_DIR"/stacks/op-scim-bridge $BUILD_DIR
+
+cd $BUILD_DIR
+
+# Remove test templates
+find "$SRC_VERSION" -type d -name tests -print0 | xargs -0 rm -rf
+
+# Create YAML directory
+rm -rf "$ROOT_DIR"/stacks/op-scim-bridge/yaml
+mkdir -p "$ROOT_DIR"/stacks/op-scim-bridge/yaml
+
+# render mginx-ingress
+helm template \
+  --name op-scim-bridge \
+  --namespace op-scim-bridge \
+  --values "$ROOT_DIR"/src/op-scim-bridge/"$SRC_VERSION"/values.yaml \
+  "$SRC_VERSION" > "$ROOT_DIR"/stacks/op-scim-bridge/yaml/op-scim-bridge.yaml
