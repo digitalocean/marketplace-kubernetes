555d093a0bb99219ba460f4edaa0e72e93eb925a
Goutham Veeramachaneni <gouthamve@gmail.com>
2019-09-24 16:46:38 +0530

    Add the loki stack YAMLs
    Signed-off-by: Goutham Veeramachaneni <gouthamve@gmail.com>

---
+++ stacks/loki/render.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+BUILD_DIR=$(mktemp -d)
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
+SRC_VERSION="0.16.2"
+
+cp -r "$ROOT_DIR"/src/loki/"$SRC_VERSION" $BUILD_DIR
+cp -r "$ROOT_DIR"/stacks/loki $BUILD_DIR
+
+cd $BUILD_DIR
+
+# Remove test templates
+find "$SRC_VERSION" -type d -name tests -print0 | xargs -0 rm -rf
+
+# Create YAML directory
+rm -rf "$ROOT_DIR"/stacks/loki/yaml
+mkdir -p "$ROOT_DIR"/stacks/loki/yaml
+
+# render loki 
+helm template \
+  --name loki \
+  --namespace loki \
+  --values "$SRC_VERSION"/values.yaml \
+  "$SRC_VERSION" > "$ROOT_DIR"/stacks/loki/yaml/loki.yaml
