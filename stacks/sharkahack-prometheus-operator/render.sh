bd8bfc23b3f51493c96a9fda3e0dfa738f8188f7
Paul Anunda <paulanunda@gmail.com>
2019-09-26 12:45:42 -0500

    updated render.sh

--- stacks/sharkahack-prometheus-operator/render.sh
+++ stacks/sharkahack-prometheus-operator/render.sh
@@ -8,7 +8,7 @@ ROOT_DIR=$(git rev-parse --show-toplevel)
 PROMETHEUS_OPERATOR_CHART_VERSION="5.12.3"
 
 cp -r "$ROOT_DIR"/src/prometheus-operator/"$PROMETHEUS_OPERATOR_CHART_VERSION" $BUILD_DIR
-cp -r "$ROOT_DIR"/stacks/prometheus-operator $BUILD_DIR
+cp -r "$ROOT_DIR"/stacks/sharkahack-prometheus-operator $BUILD_DIR
 
 cd $BUILD_DIR
 
@@ -16,12 +16,12 @@ cd $BUILD_DIR
 find "$PROMETHEUS_OPERATOR_CHART_VERSION" -type d -name tests -print0 | xargs -0 rm -rf
 
 # Create YAML directory
-rm -rf "$ROOT_DIR"/stacks/prometheus-operator/yaml
-mkdir -p "$ROOT_DIR"/stacks/prometheus-operator/yaml
+rm -rf "$ROOT_DIR"/stacks/sharkahack-prometheus-operator/yaml
+mkdir -p "$ROOT_DIR"/stacks/sharkahack-prometheus-operator/yaml
 
 # render prometheus-operator
 helm template \
   --name prometheus-operator \
   --namespace prometheus-operator \
-  --values prometheus-operator/values/prometheus-operator.yaml \
-  "$PROMETHEUS_OPERATOR_CHART_VERSION" > "$ROOT_DIR"/stacks/prometheus-operator/yaml/prometheus-operator.yaml
+  --values sharkahack-prometheus-operator/values/prometheus-operator.yaml \
+  "$PROMETHEUS_OPERATOR_CHART_VERSION" > "$ROOT_DIR"/stacks/sharkahack-prometheus-operator/yaml/prometheus-operator.yaml
