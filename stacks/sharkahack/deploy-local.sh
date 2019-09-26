dec21b7b8d3624c871b2984b259d3242132b8df2
Paul Anunda <paulanunda@gmail.com>
2019-09-26 12:53:31 -0500

    added deploy-nginx-ingress.sh

--- stacks/sharkahack/deploy-local.sh
+++ stacks/sharkahack/deploy-local.sh
@@ -4,5 +4,6 @@ set -e
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
+sh "$ROOT_DIR"/stacks/sharkahack/deploy-nginx-ingress.sh
 sh "$ROOT_DIR"/stacks/metrics-server/deploy-local.sh
 sh "$ROOT_DIR"/stacks/sharkahack-prometheus-operator/deploy-local.sh
