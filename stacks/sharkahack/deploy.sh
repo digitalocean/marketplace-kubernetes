e2f50e3ed261c65929878c9b1fc9fdb424a99ea1
Paul Anunda <paulanunda@gmail.com>
2019-09-27 09:18:17 -0500

    fixed deploy.sh

--- stacks/sharkahack/deploy.sh
+++ stacks/sharkahack/deploy.sh
@@ -2,5 +2,19 @@
 
 set -e
 
-sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/metrics-server/deploy.sh | sh"
-sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/sharkahack-prometheus-operator/deploy.sh | sh"
+DOMAIN=dohackafun.com
+
+if [ -z "${DO_API_TOKEN}" ]; then
+  echo "Error: DO_API_TOKEN not defined."
+  exit 1
+fi
+
+if [ -z "${DOMAIN}" ]; then
+  echo "Error: DOMAIN not defined."
+  exit 1
+fi
+
+sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/sharkahack/deploy-nginx-ingress.sh | sh"
+sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/metrics-server/deploy-local.sh | sh"
+sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/sharkahack-prometheus-operator/deploy-local.sh | sh"
+sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/sharkahack/shared.sh | sh"
