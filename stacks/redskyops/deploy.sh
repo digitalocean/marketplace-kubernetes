2176b181a0fe14b9ce933324c52f467c48800382
Jeremy Gustie <jeremy@carbonrelay.com>
2019-10-28 11:50:01 -0400

    Update Red Sky Ops deployment scripts to also use recommended defaults

--- stacks/redskyops/deploy.sh
+++ stacks/redskyops/deploy.sh
@@ -7,14 +7,14 @@ cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: Namespace
 metadata:
-  name: redskyops
+  name: redsky-system
 EOF
 
 # set kubectl namespace
-kubectl config set-context --current --namespace=redskyops
+kubectl config set-context --current --namespace=redsky-system
 
 # deploy redskyops
 kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/redskyops/yaml/redskyops.yaml
 
 # ensure services are running
-kubectl rollout status deployment/redskyops-controller-manager
+kubectl rollout status deployment/redsky-controller-manager
