199cd2dce552eb7938398e26e7edd477f74ccf6e
Paul Anunda <paulanunda@gmail.com>
2019-11-11 22:20:34 -0600

    cleanup

--- stacks/nginx-ingress/deploy.sh
+++ stacks/nginx-ingress/deploy.sh
@@ -4,5 +4,9 @@ set -e
 
 VERSION=nginx-0.26.1
 
+# deploy
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/$VERSION/deploy/static/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/$VERSION/deploy/static/provider/cloud-generic.yaml
+
+# ensure services are running
+kubectl rollout status deployment/nginx-ingress-controller -n ingress-nginx
