972b33a6194e4d9fb20fa6c46ad0e9545d1b6142
Mansour Behabadi <mansour@oxplot.com>
2019-12-12 22:07:25 +1100

    Update netdata image version to 1.19.0

--- stacks/netdata/render.sh
+++ stacks/netdata/render.sh
@@ -5,7 +5,7 @@ set -e
 BUILD_DIR=$(mktemp -d)
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-SRC_VERSION="1.1.11"
+SRC_VERSION="1.1.12"
 
 cp -r "$ROOT_DIR"/src/netdata/"$SRC_VERSION" $BUILD_DIR
 cp -r "$ROOT_DIR"/stacks/netdata $BUILD_DIR
