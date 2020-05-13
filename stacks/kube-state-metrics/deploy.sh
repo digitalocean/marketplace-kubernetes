80666d34e57c3ea19a3e2a4e60a469eefb113a19
dkela <dkela@digitalocean.com>
2020-05-13 15:04:04 -0400

    minor fix

--- stacks/kube-state-metrics/deploy.sh
+++ stacks/kube-state-metrics/deploy.sh
@@ -2,14 +2,21 @@
 
 set -e
 
+STACK="kube-state-metrics"
 CHART="stable/kube-state-metrics"
 CHART_VERSION="2.8.4"
 NAMESPACE="kube-system"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  values="values.yaml"
+else
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
+fi
 
 helm install "$CHART" \
   --create-namespace \
   --generate-name \
   --namespace "$NAMESPACE" \
-  --values values.yaml \
+  --values "$values" \
   --version "$CHART_VERSION" \
   --wait
