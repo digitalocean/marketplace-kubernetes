8e59fe4580f3d11414edc39f79d79893bc7933bf
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2020-05-13 09:42:18 -0400

    Bump NetData Helm Chart from v1.21.1 => v1.22.1 (#122)
    * Bump NetData Helm Chart from v1.21.1 => v1.22.1
    
    * Fix shellcheck warnings on render.sh
    
    * Bump SRC_VERSION in render.sh
    
    * Update Netdata Stack via render.sh
    
    Co-authored-by: James Mills <prologic@shortcircuit.net.au>

--- stacks/netdata/render.sh
+++ stacks/netdata/render.sh
@@ -5,12 +5,12 @@ set -e
 BUILD_DIR=$(mktemp -d)
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-SRC_VERSION="1.1.14"
+SRC_VERSION="1.1.16"
 
-cp -r "$ROOT_DIR"/src/netdata/"$SRC_VERSION" $BUILD_DIR
-cp -r "$ROOT_DIR"/stacks/netdata $BUILD_DIR
+cp -r "$ROOT_DIR"/src/netdata/"$SRC_VERSION" "$BUILD_DIR"
+cp -r "$ROOT_DIR"/stacks/netdata "$BUILD_DIR"
 
-cd $BUILD_DIR
+cd "$BUILD_DIR" || exit 1
 
 # Remove test templates
 find "$SRC_VERSION" -type d -name tests -print0 | xargs -0 rm -rf
