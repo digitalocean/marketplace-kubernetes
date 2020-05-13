1dee6a1b46d8fc1088badda677e73c7fac81f5a0
dkela <dkela@digitalocean.com>
2020-05-13 12:26:57 -0400

    logic changes

--- stacks/helm3-test/deploy.sh
+++ stacks/helm3-test/deploy.sh
@@ -5,23 +5,19 @@ set -e
 STACK="helm3-test"
 CHART="stable/kube-state-metrics"
 CHART_VERSION="2.8.4"
-NAMESPACE="kube-system"
+NAMESPACE="new"
 
 if [ -z "$MP_KUBERNETES" ]
-then 
-helm install "$CHART" \
-  --create-namespace \
-  --generate-name \
-  --namespace "$NAMESPACE" \
-  --values values.yaml \
-  --version "$CHART_VERSION" \
-  --wait
+then
+    values="values.yaml"
 else
-helm install "$CHART" \
-  --create-namespace \
-  --generate-name \
-  --namespace "$NAMESPACE" \
-  --values https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/"$STACK"/values.yaml \
-  --version "$CHART_VERSION" \
-  --wait
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/helm3-test/stacks/${STACK}/values.yaml"
 fi
+
+helm install "$CHART" \
+--create-namespace \
+--generate-name \
+--namespace "$NAMESPACE" \
+--values "$values" \
+--version "$CHART_VERSION" \
+--wait
