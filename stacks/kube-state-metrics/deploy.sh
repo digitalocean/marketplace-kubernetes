7dbafd1d0093beb8ef3c181f0ba14588ae1fe07a
dkela <dkela@digitalocean.com>
2020-05-13 15:16:32 -0400

    styling

--- stacks/kube-state-metrics/deploy.sh
+++ stacks/kube-state-metrics/deploy.sh
@@ -8,15 +8,16 @@ CHART_VERSION="2.8.4"
 NAMESPACE="kube-system"
 
 if [ -z "${MP_KUBERNETES}" ]; then
-  values="values.yaml"
+  VALUES="values.yaml"
 else
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
+  VALUES="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
 fi
 
 helm install "$CHART" \
   --create-namespace \
   --generate-name \
   --namespace "$NAMESPACE" \
-  --values "$values" \
+  --values "$VALUES" \
   --version "$CHART_VERSION" \
   --wait
+  
