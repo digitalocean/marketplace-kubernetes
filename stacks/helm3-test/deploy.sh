1c591f2f96e8d3c892dd93030bba15f15b7fd97b
Paul Anunda <paulanunda@gmail.com>
2020-05-13 12:43:24 -0400

    slight tweaks

--- stacks/helm3-test/deploy.sh
+++ stacks/helm3-test/deploy.sh
@@ -5,19 +5,18 @@ set -e
 STACK="helm3-test"
 CHART="stable/kube-state-metrics"
 CHART_VERSION="2.8.4"
-NAMESPACE="new"
+NAMESPACE="helm3-test"
 
-if [ -z "$MP_KUBERNETES" ]
-then
-    values="values.yaml"
+if [ -z "${MP_KUBERNETES}" ]; then
+  values="values.yaml"
 else
-    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
 fi
 
 helm install "$CHART" \
---create-namespace \
---generate-name \
---namespace "$NAMESPACE" \
---values "$values" \
---version "$CHART_VERSION" \
---wait
+  --create-namespace \
+  --generate-name \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION" \
+  --wait
