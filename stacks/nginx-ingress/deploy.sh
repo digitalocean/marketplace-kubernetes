d001652fdd3135604dc1ae80d017a21c5219cc98
Paul Anunda <paulanunda@gmail.com>
2020-05-14 13:15:39 -0400

    added nginx values

--- stacks/nginx-ingress/deploy.sh
+++ stacks/nginx-ingress/deploy.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
+# https://github.com/kubernetes/ingress-nginx/tree/master/charts/ingress-nginx
+
 set -e
 
+STACK="nginx-ingress"
 CHART="ingress-nginx/ingress-nginx"
 CHART_VERSION="2.1.0"
 NAMESPACE="ingress-nginx"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  VALUES="values.yaml"
+else
+  VALUES="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
+fi
 
 helm install "$CHART" \
   --create-namespace \
   --generate-name \
   --namespace "$NAMESPACE" \
+  --values "$VALUES" \
   --version "$CHART_VERSION" \
   --wait
