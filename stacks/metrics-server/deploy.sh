a19fce85b7860a902c824f4c4b2796090482130b
dkela <dkela@digitalocean.com>
2020-05-20 11:01:52 -0400

    using helm upgrade

--- stacks/metrics-server/deploy.sh
+++ stacks/metrics-server/deploy.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
+set -e
+
 STACK="metrics-server"
 CHART="stable/metrics-server"
 CHART_VERSION="2.11.1"
 NAMESPACE="kube-system"
-
-helm status $STACK -n $NAMESPACE 
-if [ $? -eq 0 ]; then
-  echo "${CHART} is already installed."
-  exit 0
-fi
 
 if [ -z "${MP_KUBERNETES}" ]; then
   VALUES="values.yaml"
@@ -17,15 +13,9 @@ else
   VALUES="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/${STACK}/values.yaml"
 fi
 
-helm install "$STACK" "$CHART" \
+helm upgrade --install "$STACK" "$CHART" \
   --create-namespace \
   --namespace "$NAMESPACE" \
   --values "$VALUES" \
   --version "$CHART_VERSION" \
   --wait
-
-if [ $? -eq 0 ]; then
-  exit 0
-else
-  exit 1
-fi
