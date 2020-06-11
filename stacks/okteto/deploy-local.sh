d816b5edbad8404cfc7fca3cd32c56112e4a19b9
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-06-11 12:10:16 -0700

    manifests for okteto 0.5.1
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- stacks/okteto/deploy-local.sh
+++ stacks/okteto/deploy-local.sh
@@ -27,7 +27,11 @@ PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
 kubectl label sa do-okteto-enterprise dev.okteto.com/token=$PASSWORD --overwrite
 
 # ensure services are running
-kubectl get deployments -o custom-columns=NAME:.metadata.name | tail -n +2 | while read -r line
-do
-  kubectl rollout status -w deployment/"$line"
-done
+kubectl rollout status -w deployment/do-cert-manager
+kubectl rollout status -w deployment/do-cert-manager-cainjector
+kubectl rollout status -w deployment/do-nginx-ingress-controller
+kubectl rollout status -w deployment/do-nginx-ingress-default-backend
+kubectl rollout status -w deployment/do-okteto-enterprise-frontend
+kubectl rollout status -w deployment/do-cert-manager-webhook
+kubectl rollout status -w deployment/do-okteto-enterprise-api
+kubectl rollout status -w deployment/do-okteto-enterprise-mutation-webhook
--- stacks/okteto/render.sh
+++ stacks/okteto/render.sh
@@ -4,11 +4,14 @@ set -e
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 
-SRC_VERSION="0.3.0"
+SRC_VERSION="0.5.1"
 
 # Create YAML directory
 rm -rf "$ROOT_DIR"/stacks/okteto/yaml
 mkdir -p "$ROOT_DIR"/stacks/okteto/yaml
 
-cp "$ROOT_DIR"/src/okteto/"$SRC_VERSION"/okteto.yaml "$ROOT_DIR"/stacks/okteto/yaml/okteto.yaml
+
+# render YAML
+helm template "do" "${ROOT_DIR}/src/okteto/${SRC_VERSION}" --namespace okteto --values values.yaml > "$ROOT_DIR"/stacks/okteto/yaml/okteto.yaml
+
 cp "$ROOT_DIR"/src/okteto/"$SRC_VERSION"/crds.yaml "$ROOT_DIR"/stacks/okteto/yaml/crds.yaml
