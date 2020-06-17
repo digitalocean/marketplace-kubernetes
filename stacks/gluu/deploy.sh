f4895c5475776a0c77366a037bea15fbd4edab75
mogluu <47318409+mogluu@users.noreply.github.com>
2020-06-17 14:10:30 -0400

    Add namespace parameter

--- stacks/gluu/deploy.sh
+++ stacks/gluu/deploy.sh
@@ -8,13 +8,13 @@ kubectl create clusterrolebinding gluu-cluster-admin \
   --serviceaccount=default:default
 
 # Install Gluu cloud native edition, KubeDB
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu.yaml
+kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu.yaml --namespace default
 # Wait for Gluu CN installation job to finish
 kubectl wait --for=condition=complete --timeout=900s job/gluu-cloud-native-installer
 # Wait for main services to run
 kubectl -n gluu wait --for=condition=available --timeout=900s deploy/gluu-oxauth
 kubectl -n gluu wait --for=condition=available --timeout=300s deploy/gluu-oxd-server
 # Install Gluu Gateway UI
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu-gateway.yaml
+kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu-gateway.yaml --namespace default
 # Wait for Gluu Gateway installation job to finish
 kubectl wait --for=condition=complete --timeout=900s job/gluu-cloud-native-installer-gluu-gateway
