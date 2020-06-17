99fa6345e41b8655d5bae2cd42cbb2e6576a2e4b
mogluu <47318409+mogluu@users.noreply.github.com>
2020-06-17 08:44:39 -0400

    Adjust names and add wait fors

--- stacks/gluu/deploy.sh
+++ stacks/gluu/deploy.sh
@@ -3,15 +3,18 @@
 set -e
 
 # Create clusterrolebbinding for default sa
-kubectl create clusterrolebinding add-on-cluster-admin \
+kubectl create clusterrolebinding gluu-cluster-admin \
   --clusterrole=cluster-admin \
   --serviceaccount=default:default
 
 # Install Gluu cloud native edition, KubeDB
 kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu.yaml
+# Wait for Gluu CN installation job to finish
+kubectl wait --for=condition=complete --timeout=900s job/gluu-cloud-native-installer
 # Wait for main services to run
 kubectl -n gluu wait --for=condition=available --timeout=900s deploy/gluu-oxauth
 kubectl -n gluu wait --for=condition=available --timeout=300s deploy/gluu-oxd-server
 # Install Gluu Gateway UI
 kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu-gateway.yaml
-
+# Wait for Gluu Gateway installation job to finish
+kubectl wait --for=condition=complete --timeout=900s job/gluu-cloud-native-installer-gluu-gateway
