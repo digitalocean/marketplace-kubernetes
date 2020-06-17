4e4dd118f1b8e1d636f3f7f87f6b5caccb9b239b
mogluu <47318409+mogluu@users.noreply.github.com>
2020-06-17 08:01:45 -0400

    Switch to using job manifest to install Gluu on DO
    The use of job manifests to install is intended to be used with 4.2. Under the current curcumstances DO 1 click app doesn't support python3 installer and hence a demo version for 4.1 will be used.

--- stacks/gluu/deploy-local.sh
+++ stacks/gluu/deploy-local.sh
@@ -2,16 +2,17 @@
 
 set -e
 
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
 # Create clusterrolebbinding for default sa
 kubectl create clusterrolebinding add-on-cluster-admin \
   --clusterrole=cluster-admin \
   --serviceaccount=default:default
 
 # Install Gluu cloud native edition, KubeDB
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu.yaml
+kubectl apply -f "$ROOT_DIR"/stacks/gluu/yaml/gluu.yaml
 # Wait for main services to run
 kubectl -n gluu wait --for=condition=available --timeout=900s deploy/gluu-oxauth
 kubectl -n gluu wait --for=condition=available --timeout=300s deploy/gluu-oxd-server
 # Install Gluu Gateway UI
-kubectl apply -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gluu/yaml/gluu-gateway.yaml
-
+=kubectl apply -f "$ROOT_DIR"/stacks/gluu/yaml/gluu-gateway.yaml
