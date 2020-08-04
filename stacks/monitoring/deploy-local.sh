3f12e4250acc62d1a8e69f6757fa2471369401b7
Paul Anunda <panunda@digitalocean.com>
2020-08-04 11:28:41 -0400

    Updated metrics-server & prometheus-operator (#150)
    * updated metrics-server
    
    * updated prometheus-operator
    
    * consolidated monitoring deploy.sh

--- stacks/monitoring/deploy-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-ROOT_DIR=$(git rev-parse --show-toplevel)
-
-sh "$ROOT_DIR"/stacks/metrics-server/deploy-local.sh
-sh "$ROOT_DIR"/stacks/prometheus-operator/deploy-local.sh
--- stacks/monitoring/deploy.sh
+++ stacks/monitoring/deploy.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/metrics-server/deploy.sh | sh"
-sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/prometheus-operator/deploy.sh | sh"
+if [ -z "${MP_KUBERNETES}" ]; then
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  sh "$ROOT_DIR"/stacks/metrics-server/deploy.sh
+  sh "$ROOT_DIR"/stacks/prometheus-operator/deploy.sh
+else
+  sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/metrics-server/deploy.sh | sh"
+  sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/prometheus-operator/deploy.sh | sh"
+fi
