62cac1807cc96eba1325836a6c206bd6a86139df
Keith <keith.mcclellan@gmail.com>
2020-11-04 14:54:04 -0500

    cockroachdb-first-pr (#171)
    * cockroachdb-first-pr
    
    initial pr for cockroachdb
    
    * Update deploy.sh
    
    * fix typo
    
    * Update deploy.sh
    
    remove --atomic
    
    * add auto-certificate approval
    
    * invert csr list and wait 10 seconds before approving
    
    * switch from tail -r to sed
    
    * increase wait time

---
+++ stacks/cockroachdb/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add cockroachdb https://charts.cockroachdb.com/
+helm repo update
+
+################################################################################
+# chart
+################################################################################
+STACK="cockroachdb"
+CHART="cockroachdb/cockroachdb"
+CHART_VERSION="4.0.7"
+NAMESPACE="cockroachdb"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/cockroachdb/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/cockroachdb/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --install \
+  --create-namespace \
+  --namespace "$NAMESPACE" \
+  --values "$values"
+
+sleep 30
+
+for i in `kubectl get csr | tail -n +2 | sed '1!G;h;$!d' | cut -d ' ' -f1 | grep cockroach`; do kubectl certificate approve $i; sleep 10; done
+
