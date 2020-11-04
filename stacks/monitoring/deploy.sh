29e34d47760506256791f3f15a93cae05e39460c
kmpartner <kmpartner@outlook.com>
2020-11-04 21:19:09 +0900

    change monitoring deploy.sh add values.yaml

--- stacks/monitoring/deploy.sh
+++ stacks/monitoring/deploy.sh
@@ -2,11 +2,79 @@
 
 set -e
 
-if [ -z "${MP_KUBERNETES}" ]; then
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  sh "$ROOT_DIR"/stacks/metrics-server/deploy.sh
-  sh "$ROOT_DIR"/stacks/prometheus-operator/deploy.sh
+helm uninstall -n prometheus-operator prometheus-operator || echo -n 'prometheus-operator not installed'
+
+kubectl delete ns prometheus-operator --ignore-not-found
+
+################################################################################
+# repo
+################################################################################
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo update
+
+################################################################################
+# chart
+################################################################################
+STACK="prometheus-operator"
+CHART="stable/prometheus-operator"
+CHART_VERSION="9.3.0"
+NAMESPACE="prometheus-operator"
+
+# if [ -z "${MP_KUBERNETES}" ]; then
+#   # use local version of values.yml
+#   ROOT_DIR=$(git rev-parse --show-toplevel)
+#   values="$ROOT_DIR/stacks/prometheus-operator/values.yml"
+# else
+#   # use github hosted master version of values.yml
+#   values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/prometheus-operator/values.yml"
+# fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --install \
+  --create-namespace \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION" \
+  --values "values.yaml"  \
+
+
+# -----
+# prometheus-operator
+# grafana login ID: admin, password: prom-operator
+
+
+# check if metrics-server is already installed
+CHECK=$(kubectl get svc metrics-server -n kube-system --ignore-not-found)
+if [ "$CHECK" = "" ]
+then
+  echo "metrics-server not found"
 else
-  sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/metrics-server/deploy.sh | sh"
-  sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/prometheus-operator/deploy.sh | sh"
+  echo "metrics-server found, exiting"
+  exit 0
 fi
+
+################################################################################
+# repo
+################################################################################
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+################################################################################
+# chart
+################################################################################
+STACK="metrics-server"
+CHART="bitnami/metrics-server"
+CHART_VERSION="4.3.0"
+NAMESPACE="kube-system"
+
+# if [ -z "${MP_KUBERNETES}" ]; then
+#   # use local version of values.yml
+#   ROOT_DIR=$(git rev-parse --show-toplevel)
+#   values="$ROOT_DIR/stacks/metrics-server/values.yml"
+# else
+#   # use github hosted master version of values.yml
+#   values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/metrics-server/values.yml"
+# fi
+
+helm upgrade --install "$STACK" "$CHART" --atomic --namespace "$NAMESPACE" --version "$CHART_VERSION" -f metrics-server-values.yaml
