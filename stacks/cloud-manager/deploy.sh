8fc73368505a32c2184b07c66195051b5e6cd089
Jorge Simao <jsimao71@gmail.com>
2020-11-18 18:12:31 +0000

    added einnovator cloud-manager stack based on chart einnovator-devops

---
+++ stacks/cloud-manager/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add ei https://cdn.einnovator.org/charts
+helm repo update
+
+################################################################################
+# chart
+################################################################################
+STACK="cloud-manager"
+CHART="ei/einnovator-devops"
+CHART_VERSION="1.0.0"
+NAMESPACE="einnovator"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/cloud-manager/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/cloud-manager/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --install \
+  --atomic \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
