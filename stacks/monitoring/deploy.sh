fd1f91ea8c15773b9718b8b1d2ab837b21f4d571
Paul Anunda <panunda@digitalocean.com>
2020-12-08 14:32:12 -0500

    added kube-prometheus-stack (#183)

--- stacks/monitoring/deploy.sh
+++ stacks/monitoring/deploy.sh
@@ -5,8 +5,8 @@ set -e
 if [ -z "${MP_KUBERNETES}" ]; then
   ROOT_DIR=$(git rev-parse --show-toplevel)
   sh "$ROOT_DIR"/stacks/metrics-server/deploy.sh
-  sh "$ROOT_DIR"/stacks/prometheus-operator/deploy.sh
+  sh "$ROOT_DIR"/stacks/kube-prometheus-stack/deploy.sh
 else
   sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/metrics-server/deploy.sh | sh"
-  sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/prometheus-operator/deploy.sh | sh"
+  sh -c "curl --location --silent --show-error https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/kube-prometheus-stack/deploy.sh | sh"
 fi
