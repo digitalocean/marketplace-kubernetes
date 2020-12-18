b30e912cd009ffb9c73f0114e981e029f99c9a74
David Young <davidy@funkypenguin.co.nz>
2020-12-19 04:17:32 +1300

    Add portainer (#184)

---
+++ stacks/portainer/README.md
@@ -0,0 +1,5 @@
+# Portainer
+
+See [here](https://github.com/portainer/k8s/blob/master/charts/portainer/README.md) for details re chart values.
+
+Unlike a default deployment directly from the helm chart, this deployment defaults to "LoadBalancer" mode (since DO Kubernetes is known to support load-balancing).
---
+++ stacks/portainer/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add portainer https://portainer.github.io/k8s/
+helm repo update
+
+################################################################################
+# chart
+################################################################################
+STACK="portainer"
+CHART="portainer/portainer"
+CHART_VERSION="1.0.x"
+NAMESPACE="portainer"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/portainer/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/portainer/values.yml"
+fi
+
+# Create the namespace if it doesn't already exist
+kubectl create namespace portainer -o yaml --dry-run | kubectl apply -f -
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --install \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
