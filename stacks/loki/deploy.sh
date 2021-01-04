62f80deda56e33a53d191e269ef997b4e1645a17
Paul Anunda <panunda@digitalocean.com>
2021-01-04 11:06:34 -0500

    added loki-stack

---
+++ stacks/loki/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+################################################################################
+# chart
+################################################################################
+STACK="loki"
+CHART="grafana/loki-stack"
+CHART_VERSION="2.3.1"
+NAMESPACE="loki-stack"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/loki/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/loki/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
