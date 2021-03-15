21dd08eed9a1b3d9f55c1d5b579fe3f15f00b343
Paul Anunda <panunda@digitalocean.com>
2021-03-15 10:34:38 -0400

    updated ingress-nginx

--- stacks/ingress-nginx/deploy.sh
+++ stacks/ingress-nginx/deploy.sh
@@ -2,33 +2,4 @@
 
 set -e
 
-################################################################################
-# repo
-################################################################################
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update > /dev/null
-
-################################################################################
-# chart
-################################################################################
-STACK="ingress-nginx"
-CHART="ingress-nginx/ingress-nginx"
-CHART_VERSION="3.20.1"
-NAMESPACE="ingress-nginx"
-
-if [ -z "${MP_KUBERNETES}" ]; then
-  # use local version of values.yml
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  values="$ROOT_DIR/stacks/ingress-nginx/values.yml"
-else
-  # use github hosted master version of values.yml
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/ingress-nginx/values.yml"
-fi
-
-helm upgrade "$STACK" "$CHART" \
-  --atomic \
-  --create-namespace \
-  --install \
-  --namespace "$NAMESPACE" \
-  --values "$values" \
-  --version "$CHART_VERSION"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/do/deploy.yaml
