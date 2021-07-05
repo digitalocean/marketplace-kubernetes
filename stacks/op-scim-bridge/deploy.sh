b68b51ea76e69d8d8b4cb0924af1d295dd49da3c
De Ville Weppenaar <deville.weppenaar@agilebits.com>
2021-07-05 14:07:07 -0700

    update op-scim-bridge/op-scim to v2.1.0

--- stacks/op-scim-bridge/deploy.sh
+++ stacks/op-scim-bridge/deploy.sh
@@ -7,7 +7,7 @@ helm repo update > /dev/null
 
 STACK="op-scim-bridge"
 CHART="op-scim-bridge/op-scim"
-CHART_VERSION="2.0.2"
+CHART_VERSION="2.1.0"
 NAMESPACE="op-scim-bridge"
 
 helm upgrade "$STACK" "$CHART" \
