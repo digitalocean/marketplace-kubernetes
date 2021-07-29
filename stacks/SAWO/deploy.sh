15a3f2504a73436dc8f55486b773a4b55639a6a2
akuadvi <akuadvi@gmail.com>
2021-07-29 13:09:43 +0530

    SAWO added under stacks

---
+++ stacks/SAWO/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add stable https://charts.helm.sh/stable
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="SAWO"
+CHART="SAWO/SAWO"
+CHART_VERSION="1.0.0"
+NAMESPACE="SAWO"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/SAWO/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/SAWO/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
