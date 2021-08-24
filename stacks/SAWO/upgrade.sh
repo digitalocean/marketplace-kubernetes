19f2870de30737796186c5b374f67ccb3aefbeaa
akuadvi <48834329+akuadvi@users.noreply.github.com>
2021-08-24 11:10:49 +0530

    Update upgrade.sh

--- stacks/SAWO/upgrade.sh
+++ stacks/SAWO/upgrade.sh
@@ -11,17 +11,17 @@ helm repo update > /dev/null
 ################################################################################
 # chart
 ################################################################################
-STACK="SAWO"
-CHART="SAWO/SAWO"
-NAMESPACE="SAWO"
+STACK="sawo"
+CHART="sawo/sawo"
+NAMESPACE="sawo"
 
 if [ -z "${MP_KUBERNETES}" ]; then
     # use local version of values.yml
     ROOT_DIR=$(git rev-parse --show-toplevel)
-    values="$ROOT_DIR/stacks/SAWO/values.yml"
+    values="$ROOT_DIR/stacks/sawo/values.yml"
 else
     # use github hosted master version of values.yml
-    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/SAWO/values.yml"
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/sawo/values.yml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
