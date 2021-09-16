c97d44ac13fb95c9b3fb237e6ba66acae7845377
Vishnu Narayanan <vishnu@chatwoot.com>
2021-09-16 14:59:16 +0530

    add chatwoot

---
+++ stacks/chatwoot/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add chatwoot https://chatwoot.github.io/charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="chatwoot"
+CHART="chatwoot/chatwoot"
+NAMESPACE="chatwoot"
+CHART_VERSION="0.6.4"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/chatwoot/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/chatwoot/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
---
+++ stacks/chatwoot/upgrade.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add chatwoot https://chatwoot.github.io/charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="chatwoot"
+CHART="chatwoot/chatwoot"
+NAMESPACE="chatwoot"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/chatwoot/values.yml"
+else
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/chatwoot/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+--namespace "$NAMESPACE" \
+--values "$values" \
