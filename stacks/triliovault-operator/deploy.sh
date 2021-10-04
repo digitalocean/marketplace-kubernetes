841638b34c28643bfe08618a15704f03c1c1f035
Paul Anunda <paulanunda@gmail.com>
2021-10-04 17:12:21 -0400

    triliovault deploy.sh fix

--- stacks/triliovault-operator/deploy.sh
+++ stacks/triliovault-operator/deploy.sh
@@ -15,14 +15,16 @@ STACK="triliovault-operator"
 CHART="triliovault-operator/k8s-triliovault-operator"
 CHART_VERSION="2.1.0"
 NAMESPACE="tvk"
-ROOT_DIR=$(git rev-parse --show-toplevel)
 
 if [ -z "${MP_KUBERNETES}" ]; then
   # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
   values="$ROOT_DIR/stacks/triliovault-operator/values.yml"
+  TVM="$ROOT_DIR/stacks/$STACK/triliovault-manager.yaml"
 else
   # use github hosted master version of values.yml
   values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/triliovault-operator/values.yml"
+  TVM="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/triliovault-operator/triliovault-manager.yaml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
@@ -33,14 +35,6 @@ helm upgrade "$STACK" "$CHART" \
   --values "$values" \
   --version "$CHART_VERSION"
 
-if [ -z "${MP_KUBERNETES}" ]; then
-  TVM="$ROOT_DIR/stacks/$STACK/triliovault-manager.yaml"
-else
-  TVM="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/triliovault-operator/triliovault-manager.yaml"
-fi
-
 kubectl apply -f "$TVM" --namespace "$NAMESPACE"
 
 until (kubectl get pods -n "$NAMESPACE" -l "triliovault.trilio.io/owner=triliovault-manager" 2>/dev/null | grep Running); do sleep 3; done
-sleep 5
-
