85b9e94be142aa1aa56f1cd04daa8c15205baadf
David Delassus <david.jose.delassus@gmail.com>
2021-10-13 21:58:28 +0200

    Add kapp-controller stack (#270)
    * Add kapp-controller stack
    
    * Update file permissions in kapp-controller stack
    
    * Fix CHART variable in stack kapp-controller

---
+++ stacks/kapp-controller/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add link-society-incubator https://charts.link-society.com/incubator
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="kapp-controller"
+CHART="link-society-incubator/kapp-controller"
+CHART_VERSION="0.1.0"
+NAMESPACE="kapp-controller"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/kapp-controller/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/kapp-controller/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
