7ab2c993e588181c324344e17618b1beec719f75
root <root@bh-ubuntu2004-vm.c.tvk-solutions-330321.internal>
2022-01-10 18:29:14 +0000

    TVK pathc update v2.6.5

--- stacks/triliovault-operator/deploy.sh
+++ stacks/triliovault-operator/deploy.sh
@@ -13,7 +13,7 @@ helm repo update > /dev/null
 ################################################################################
 STACK="triliovault-operator"
 CHART="triliovault-operator/k8s-triliovault-operator"
-CHART_VERSION="2.6.4"
+CHART_VERSION="2.6.5"
 NAMESPACE="tvk"
 #HOME=$ROOT_DIR
 
