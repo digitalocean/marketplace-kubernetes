b3dd0b7f87f5f8e039a4da7a34f18826d4377faa
root <root@bh-ubuntu2004-vm.c.tvk-solutions-330321.internal>
2022-01-27 10:12:08 +0000

    TVK update 2.6.7

--- stacks/triliovault-operator/uninstall.sh
+++ stacks/triliovault-operator/uninstall.sh
@@ -16,8 +16,14 @@ echo "Deleting TVK License ..."
 kubectl delete license --all --namespace "$NAMESPACE"
 
 echo ""
+echo "Looking for TVM custom resource name"
+TVM_CR=$(kubectl get tvm --no-headers=true --namespace "$NAMESPACE" 2>/dev/null | awk '{print $1}')
+echo "$TVM_CR"
+TVM_NAME=$(kubectl get tvm "$TVM_CR" --namespace "$NAMESPACE" -o jsonpath='{.metadata.name}')
+echo "$TVM_NAME"
+
 echo "Deleting TVK Manager ..."
-kubectl delete -f "$TVM" --namespace "$NAMESPACE"
+kubectl delete tvm "$TVM_NAME" --namespace "$NAMESPACE"
 
 echo ""
 echo "Deleting TVK Operator chart ..."
