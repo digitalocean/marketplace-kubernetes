d4300865f9f07806b07cc8196e087b4eadfb47fd
Mauricio Vargas <mavargas11@uc.cl>
2022-02-01 11:58:12 -0500

    permissions

--- stacks/rstudio/deploy.sh
+++ stacks/rstudio/deploy.sh
@@ -3,8 +3,9 @@
 set -e
 
 # set kubectl namespace
+kubectl create namespace rstudio
 kubectl config set-context --current --namespace=rstudio
 
-# deploy netdata
+# deploy rstudio
 kubectl apply -f rstudio.yaml
 
