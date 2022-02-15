e77ef9dee05d5596a42d42385e8fb658ba5613d7
Cristian Marius Tiutiu <v-ctiutiu@digitalocean.com>
2022-02-15 19:05:04 +0200

    Nginx application version 1.1.0, stack scripts and main README

--- stacks/ingress-nginx/upgrade.sh
+++ stacks/ingress-nginx/upgrade.sh
@@ -13,24 +13,17 @@ helm repo update > /dev/null
 ################################################################################
 STACK="ingress-nginx"
 CHART="ingress-nginx/ingress-nginx"
-CHART_VERSION="4.0.13"
 NAMESPACE="ingress-nginx"
 
 if [ -z "${MP_KUBERNETES}" ]; then
-  # use local version of values.yml
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  values="$ROOT_DIR/stacks/ingress-nginx/values.yml"
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/ingress-nginx/values.yml"
 else
-  # use github hosted master version of values.yml
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/ingress-nginx/values.yml"
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/ingress-nginx/values.yml"
 fi
 
-# A timeout of 10m is needed for the Nginx Helm installation, due to the fact that DO load balancers may take a while to spin up
 helm upgrade "$STACK" "$CHART" \
-  --atomic \
-  --create-namespace \
-  --install \
-  --namespace "$NAMESPACE" \
-  --values "$values" \
-  --version "$CHART_VERSION" \
-  --timeout 10m0s
+--namespace "$NAMESPACE" \
+--values "$values" \
