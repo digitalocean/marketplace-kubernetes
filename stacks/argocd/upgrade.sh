5c66ec8771fe1df794abc254b1667fc0da4fb551
Cristian Marius Tiutiu <87807677+v-ctiutiu@users.noreply.github.com>
2022-04-06 22:00:18 +0300

    Argo CD updates and assets (#357)

--- stacks/argocd/upgrade.sh
+++ stacks/argocd/upgrade.sh
@@ -13,23 +13,17 @@ helm repo update > /dev/null
 ################################################################################
 STACK="argocd"
 CHART="argo/argo-cd"
-CHART_VERSION="4.2.1"
 NAMESPACE="argocd"
 
 if [ -z "${MP_KUBERNETES}" ]; then
-  # use local version of values.yml
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  values="$ROOT_DIR/stacks/argocd/values.yml"
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/argocd/values.yml"
 else
-  # use github hosted master version of values.yml
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/argocd/values.yml"
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/argocd/values.yml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
-  --atomic \
-  --create-namespace \
-  --install \
-  --timeout 8m0s \
-  --namespace "$NAMESPACE" \
-  --values "$values" \
-  --version "$CHART_VERSION"
+    --namespace "$NAMESPACE" \
+    --values "$values"
