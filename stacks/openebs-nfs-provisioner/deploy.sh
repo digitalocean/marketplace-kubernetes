91794cc40299b784aefea7193d32dcf9c627f192
Bikram Gupta <bikram.gupta@gmail.com>
2022-05-23 07:57:02 -0700

    Update deploy.sh (#371)
    * Update deploy.sh
    
    Change the storage class manifest to use a computed path based on the `MP_KUBERNETES` env variable (just as `values.yaml` path is computed - more portable.)
    
    * Fixed all script issues - resource path was wrong for the Helm values file as well.
    
    Co-authored-by: Cristian Marius Tiutiu <v-ctiutiu@digitalocean.com>

--- stacks/openebs-nfs-provisioner/deploy.sh
+++ stacks/openebs-nfs-provisioner/deploy.sh
@@ -15,15 +15,18 @@ STACK="openebs-nfs-provisioner"
 CHART="openebs-nfs/nfs-provisioner"
 CHART_VERSION="0.9.0"
 NAMESPACE="openebs-nfs-provisioner"
-STORAGE_CLASS_MANIFEST="assets/manifests/sc-rwx.yaml"
 
 if [ -z "${MP_KUBERNETES}" ]; then
   # use local version of values.yml
   ROOT_DIR=$(git rev-parse --show-toplevel)
   values="$ROOT_DIR/stacks/openebs-nfs-provisioner/values.yml"
+  # use local version of sc-rwx.yaml
+  STORAGE_CLASS_MANIFEST="$ROOT_DIR/stacks/openebs-nfs-provisioner/assets/manifests/sc-rwx.yaml"
 else
   # use github hosted master version of values.yml
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/openebs-nfs/values.yml"
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/openebs-nfs-provisioner/values.yml"
+  # use github hosted master version of sc-rwx.yaml
+  STORAGE_CLASS_MANIFEST="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/openebs-nfs-provisioner/assets/manifests/sc-rwx.yaml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
