24ca0cb595d6e569a9260b93a0fb049153d60678
Cristian Marius Tiutiu <v-ctiutiu@digitalocean.com>
2022-05-23 17:51:17 +0300

    fixed all script issues - resources path was wrong

--- stacks/openebs-nfs-provisioner/upgrade.sh
+++ stacks/openebs-nfs-provisioner/upgrade.sh
@@ -11,19 +11,19 @@ helm repo update > /dev/null
 ################################################################################
 # chart
 ################################################################################
-STACK="openebs-nfs"
+STACK="openebs-nfs-provisioner"
 CHART="openebs-nfs/nfs-provisioner"
-NAMESPACE="openebs"
+NAMESPACE="openebs-nfs-provisioner"
 
 if [ -z "${MP_KUBERNETES}" ]; then
     # use local version of values.yml
     ROOT_DIR=$(git rev-parse --show-toplevel)
-    values="$ROOT_DIR/stacks/openebs-nfs/values.yml"
+    values="$ROOT_DIR/stacks/openebs-nfs-provisioner/values.yml"
 else
     # use github hosted master version of values.yml
-    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/openebs-nfs/values.yml"
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/openebs-nfs-provisioner/values.yml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
---namespace "$NAMESPACE" \
---values "$values" \
+    --namespace "$NAMESPACE" \
+    --values "$values"
