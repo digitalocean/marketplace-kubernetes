4e08c1200dedb624c564231d32f9d1213730a554
Dimitris Karakasilis <gogs@fake.local>
2022-06-14 18:41:05 +0300

    Bump installed epinio to 0.11.0 (latest release)
    and fix deploy.sh and upgrade.sh scripts.
    It turns out that common.sh wasn't available next to the deploy.sh
    script so it couldn't be sourced. We opted for a simpler (and less DRY)
    approach.

--- stacks/epinio/upgrade.sh
+++ stacks/epinio/upgrade.sh
@@ -2,18 +2,81 @@
 
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-. "$SCRIPT_DIR/common.sh"
+STACK="epinio"
+CHART="epinio/epinio"
+CHART_VERSION="0.11.0"
+TRAEFIK_CHART_VERSION="10.20.1"
+CERT_MANAGER_CHART_VERSION="1.7.2"
+
+EPINIO_NAMESPACE="epinio"
+TRAEFIK_NAMESPACE="traefik"
+CERT_MANAGER_NAMESPACE="cert-manager"
+
+get_values () {
+  values=""
+  if [ -z "$MP_KUBERNETES" ]; then
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/epinio/values.yml"
+  else
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/epinio/values.yml"
+  fi
+
+  echo "$values"
+}
+
+update_repos () {
+  helm repo add epinio https://epinio.github.io/helm-charts
+  helm repo add jetstack https://charts.jetstack.io
+  helm repo add traefik https://helm.traefik.io/traefik
+  helm repo update > /dev/null
+}
+
+install_traefik () {
+  helm upgrade traefik \
+    traefik/traefik \
+    --create-namespace \
+    --install \
+    --namespace "$TRAEFIK_NAMESPACE" \
+    --version "$TRAEFIK_CHART_VERSION" \
+    --set globalArguments='' \
+    --set-string ports.web.redirectTo=websecure \
+    --set-string ingressClass.enabled=true \
+    --set-string ingressClass.isDefaultClass=true \
+    --wait
+}
+
+install_cert_manager () {
+  helm upgrade cert-manager \
+    jetstack/cert-manager \
+    --create-namespace \
+    --install \
+    --namespace "$CERT_MANAGER_NAMESPACE" \
+    --version "$CERT_MANAGER_CHART_VERSION" \
+    --set installCRDs=true \
+    --set extraArgs[0]=--enable-certificate-owner-ref=true
+}
+
+upgrade_epinio () {
+  helm upgrade "$STACK" "$CHART" \
+    --atomic \
+    --namespace "$EPINIO_NAMESPACE" \
+    --values "$1" \
+    --version "$CHART_VERSION"
+}
+
+# Script execution starts here
 
 update_repos
 
 EPINIO_VALUES=$(get_values)
 export EPINIO_VALUES
 
-echo "Upgrading traefik"
+echo "Installing traefik"
 install_traefik
 
-echo "Upgrading CertManager"
+echo "Installing CertManager"
 install_cert_manager
 
 echo "Upgrading $STACK"
