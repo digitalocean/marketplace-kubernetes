5377d6ff4f52b07ebde921f6d135bf9e0b4dde39
techmaharaj <sharma.rockstar@gmail.com>
2022-09-07 04:53:54 +0530

    Added Paralus Stack

---
+++ stacks/paralus/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -x
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add stable https://paralus.github.io/helm-charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="paralus"
+CHART="paralus/ztka"
+CHART_VERSION="0.1.0"
+NAMESPACE="paralus"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/paralus/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/paralus/helm-charts/main/examples/values.dev-generic.yaml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --timeout 8m0s \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
