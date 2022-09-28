ce0d16b2d1a5c0c93253758370725a2270fe38a2
Atulpriya Sharma <sharma.rockstar@gmail.com>
2022-09-28 23:20:55 +0530

    Adding Paralus to DigitalOcean Marketplace (#410)
    * Added Paralus Stack
    
    * Updated paralus version number
    
    * Added Readme
    
    * Added Paralus Readme
    
    * Update chart version

---
+++ stacks/paralus/upgrade.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add stable https://paralus.github.io/helm-charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="paralus"
+CHART="paralus/ztka"
+NAMESPACE="paralus"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/paralus/values.yml"
+else
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/paralus/helm-charts/main/examples/values.dev-generic.yaml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+--namespace "$NAMESPACE" \
+--values "$values" \
