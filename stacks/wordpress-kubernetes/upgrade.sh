2eab1bc0523ee5728fbafc250a9ee8c6afb7da40
Gutium Alexei <alexey.lakris@gmail.com>
2022-12-14 18:34:17 +0200

    Rename wordpress folder (#422)
    * Rename wordpress folder
    
    * Fix missed links to this repo
    
    * Fix instance naming to make it less confusing
    
    Co-authored-by: Gutium Alexei <v-agutium@digitalocean.com>

--- stacks/wordpress-kubernetes/upgrade.sh
+++ stacks/wordpress-kubernetes/upgrade.sh
@@ -11,22 +11,22 @@ helm repo update > /dev/null
 ################################################################################
 # chart
 ################################################################################
-STACK="wordpress"
+STACK="wordpress-kubernetes"
 CHART="bitnami/wordpress"
 NAMESPACE="wordpress"
 
 if [ -z "${MP_KUBERNETES}" ]; then
   # use local version of values.yml
   ROOT_DIR=$(git rev-parse --show-toplevel)
-  values="$ROOT_DIR/stacks/wordpress/values.yml"
+  values="$ROOT_DIR/stacks/wordpress-kubernetes/values.yml"
 else
   # use github hosted master version of values.yml
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/wordpress/values.yml"
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/wordpress-kubernetes/values.yml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
   --namespace "$NAMESPACE" \
   --values "$values" \
-  --set wordpressPassword=$(kubectl get secret --namespace "wordpress" wordpress -o jsonpath="{.data.wordpress-password}" | base64 --decode) \
-  --set mariadb.auth.rootPassword=$(kubectl get secret --namespace "wordpress" wordpress-mariadb -o jsonpath="{.data.mariadb-root-password}" | base64 --decode) \
-  --set mariadb.auth.password=$(kubectl get secret --namespace "wordpress" wordpress-mariadb -o jsonpath="{.data.mariadb-password}" | base64 --decode)
+  --set wordpressPassword=$(kubectl get secret --namespace "wordpress" wordpress-kubernetes -o jsonpath="{.data.wordpress-password}" | base64 --decode) \
+  --set mariadb.auth.rootPassword=$(kubectl get secret --namespace "wordpress" wordpress-kubernetes-mariadb -o jsonpath="{.data.mariadb-root-password}" | base64 --decode) \
+  --set mariadb.auth.password=$(kubectl get secret --namespace "wordpress" wordpress-kubernetes-mariadb -o jsonpath="{.data.mariadb-password}" | base64 --decode)
