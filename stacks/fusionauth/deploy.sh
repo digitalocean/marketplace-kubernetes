6d61158af0523268f51d8376311c174124f37760
Mark Manes <mark.manes@fusionauth.io>
2023-01-18 09:30:59 -0700

    Fix search-values.yaml paths for both local and marketplace deployment environment.s

--- stacks/fusionauth/deploy.sh
+++ stacks/fusionauth/deploy.sh
@@ -22,10 +22,12 @@ DB_FUSIONAUTH_USER_PASSWORD=`cat /dev/urandom | tr -dc '[:alnum:]' | head -c 42`
 if [ -z "${MP_KUBERNETES}" ]; then
   # use local version of values.yml
   ROOT_DIR=$(git rev-parse --show-toplevel)
-  values="$ROOT_DIR/stacks/fusionauth/values.yml"
+  VALUES="$ROOT_DIR/stacks/fusionauth/values.yml"
+  SEARCH_VALUES="$ROOT_DIR/stacks/fusionauth/search-values.yaml"
 else
   # use github hosted master version of values.yml
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/fusionauth/values.yml"
+  VALUES="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/fusionauth/values.yml"
+  SEARCH_VALUES="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/fusionauth/search-values.yaml"
 fi
 
 # Add repos and update
@@ -36,7 +38,7 @@ helm repo update > /dev/null
 
 # Install PostgresSQL and Elasticsearch
 helm install db bitnami/postgresql --create-namespace --namespace "$NAMESPACE" --set auth.enablePostgresUser=true --set auth.postgresPassword="$DB_POSTGRES_USER_PASSWORD" --set image.tag=14.6.0-debian-11-r11
-helm install search bitnami/elasticsearch --namespace "$NAMESPACE" -f search-values.yaml
+helm install search bitnami/elasticsearch --namespace "$NAMESPACE" -f "$SEARCH_VALUES"
 
 
 helm upgrade "$STACK" "$CHART" \
@@ -44,7 +46,7 @@ helm upgrade "$STACK" "$CHART" \
   --install \
   --timeout 8m0s \
   --namespace "$NAMESPACE" \
-  --values "$values" \
+  --values "$VALUES" \
   --version "$CHART_VERSION" \
   --set database.password="$DB_FUSIONAUTH_USER_PASSWORD" \
   --set database.root.password="$DB_POSTGRES_USER_PASSWORD"
