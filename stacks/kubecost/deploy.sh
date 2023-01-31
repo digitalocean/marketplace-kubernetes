612978c5f0d48332e0fb719cede146a51b930fdf
Linh Lam <linh@kubecost.com>
2023-01-31 14:36:16 -0800

    Kubecost first pr to list on DO Marketplace
    Signed-off-by: Linh Lam <linh@kubecost.com>

---
+++ stacks/kubecost/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add cost-analyzer https://kubecost.github.io/cost-analyzer/
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="kubecost"
+CHART="kubecost/cost-analyzer"
+CHART_VERSION="1.99.0"
+NAMESPACE="kubecost"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/kubecost/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/kubecost/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --timeout 8m0s \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
