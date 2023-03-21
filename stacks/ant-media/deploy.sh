3ad4ceb70203e1c0755b9daa02119b56b0e58c4f
Gutium Alexei <alexey.lakris@gmail.com>
2023-03-21 18:53:32 +0200

    Rename antmedia to ant-media to fix small UI issue (#445)
    * Rename antmedia to ant-media to fix small UI issue
    
    * Remove old folder
    
    ---------
    
    Co-authored-by: Gutium Alexei <v-agutium@digitalocean.com>

--- stacks/ant-media/deploy.sh
+++ stacks/ant-media/deploy.sh
@@ -13,17 +13,24 @@ helm repo update > /dev/null
 ################################################################################
 STACK="antmedia"
 CHART="antmedia/antmedia"
+CHART_VERSION="2.5.3"
 NAMESPACE="antmedia"
 
 if [ -z "${MP_KUBERNETES}" ]; then
-    # use local version of values.yml
-    ROOT_DIR=$(git rev-parse --show-toplevel)
-    values="$ROOT_DIR/stacks/antmedia/values.yml"
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/ant-media/values.yml"
 else
-    # use github hosted master version of values.yml
-    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/antmedia/values.yml"
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/ant-media/values.yml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
---namespace "$NAMESPACE" \
---values "$values" \
+  --create-namespace \
+  --install \
+  --timeout 8m0s \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION" \
+  --set edge=origin.localhost \
+  --set origin=edge.localhost
--- stacks/ant-media/upgrade.sh
+++ stacks/ant-media/upgrade.sh
@@ -13,24 +13,17 @@ helm repo update > /dev/null
 ################################################################################
 STACK="antmedia"
 CHART="antmedia/antmedia"
-CHART_VERSION="2.5.3"
 NAMESPACE="antmedia"
 
 if [ -z "${MP_KUBERNETES}" ]; then
-  # use local version of values.yml
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  values="$ROOT_DIR/stacks/antmedia/values.yml"
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/ant-media/values.yml"
 else
-  # use github hosted master version of values.yml
-  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/antmedia/values.yml"
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/ant-media/values.yml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
-  --create-namespace \
-  --install \
-  --timeout 8m0s \
-  --namespace "$NAMESPACE" \
-  --values "$values" \
-  --version "$CHART_VERSION" \
-  --set edge=origin.localhost \
-  --set origin=edge.localhost
+--namespace "$NAMESPACE" \
+--values "$values" \
