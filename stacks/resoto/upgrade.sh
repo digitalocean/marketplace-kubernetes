2f9ccdcb2734105649464b817e93e186e2b85f50
Matthias Veit <matthias_veit@yahoo.de>
2023-04-04 10:17:10 +0200

    incorporate review feedback

--- stacks/resoto/upgrade.sh
+++ stacks/resoto/upgrade.sh
@@ -14,7 +14,15 @@ helm repo update > /dev/null
 STACK="resoto"
 CHART="someengineering/resoto"
 NAMESPACE="resoto"
-values=values.yaml
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/resoto/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/resoto/values.yml"
+fi
 
 helm upgrade "$STACK" "$CHART" \
 --namespace "$NAMESPACE" \
