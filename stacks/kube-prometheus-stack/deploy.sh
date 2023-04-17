fe67ea73ad41de32acfe38c52ba0ba1eafbe1b3f
Gutium Alexei <v-agutium@digitalocean.com>
2023-04-17 21:07:51 +0300

    Upgrade metrics-server and kube-prometheus-stack to lastest versions

--- stacks/kube-prometheus-stack/deploy.sh
+++ stacks/kube-prometheus-stack/deploy.sh
@@ -13,7 +13,7 @@ helm repo update > /dev/null
 ################################################################################
 STACK="kube-prometheus-stack"
 CHART="prometheus-community/kube-prometheus-stack"
-CHART_VERSION="35.5.1"
+CHART_VERSION="45.10.1"
 NAMESPACE="kube-prometheus-stack"
 
 if [ -z "${MP_KUBERNETES}" ]; then
@@ -23,6 +23,11 @@ if [ -z "${MP_KUBERNETES}" ]; then
 else
   # use github hosted master version of values.yml
   values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/kube-prometheus-stack/values.yml"
+fi
+
+if helm history "${STACK}" -n "${NAMESPACE}" | grep -q 'pending-install'; then
+   echo "another installation is in progress, sleeping for 30 seconds"
+   sleep 30
 fi
 
 helm upgrade "$STACK" "$CHART" \
