43a57111afa87e8aab7c3d1007499fed64f5a1a0
SorokinaVeronika <sorokina.veronika91@gmail.com>
2023-04-27 11:49:57 -0700

    fixes

--- stacks/victoria-metrics-cluster/uninstall.sh
+++ stacks/victoria-metrics-cluster/uninstall.sh
@@ -7,17 +7,3 @@ NAMESPACE="default"
 
 helm uninstall "$STACK" \
   --namespace "$NAMESPACE"
-
-# Uninstall VictoriaMetrics Cluster
-if [ -z "${MP_KUBERNETES}" ]; then
-  # with local version of values.yml
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  VMCLUSTER="$ROOT_DIR/stacks/victoria-metrics-cluster/yaml/vmcluster.yaml"
-  VMAGENT="$ROOT_DIR/stacks/victoria-metrics-cluster/yaml/vmagent.yaml"
-  kubectl delete -f "$VMCLUSTER"
-  kubectl delete -f "$VMAGENT"
-else
-  # with github hosted master version of values.yml
-  kubectl delete -n default -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/victoria-metrics-cluster/yaml/vmcluster.yaml
-  kubectl delete -n default -f https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/victoria-metrics-cluster/yaml/vmagent.yaml
-fi
