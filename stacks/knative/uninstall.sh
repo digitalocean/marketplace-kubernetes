c0856dd0c29f78d10b521391b0be58ec0225358e
Gutium Alexei <v-agutium@digitalocean.com>
2023-05-04 21:27:58 +0300

    Fix uninstall

--- stacks/knative/uninstall.sh
+++ stacks/knative/uninstall.sh
@@ -5,6 +5,7 @@ set -e
 OPERATOR_VERSION="1.10.0"
 
 kubectl delete --ignore-not-found=true -f "https://github.com/knative/serving/releases/download/knative-v${OPERATOR_VERSION}/serving-crds.yaml"
+#kubectl delete --ignore-not-found=true -f "https://github.com/knative/serving/releases/download/knative-v${OPERATOR_VERSION}/serving-core.yaml"
 kubectl delete --ignore-not-found=true -f "https://github.com/knative/net-kourier/releases/download/knative-v${OPERATOR_VERSION}/kourier.yaml"
 
 kubectl delete --ignore-not-found=true -f "https://github.com/knative/eventing/releases/download/knative-v${OPERATOR_VERSION}/eventing-crds.yaml"
