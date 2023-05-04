709139b7c0e9186a314d331b44be5c44b185367f
Gutium Alexei <v-agutium@digitalocean.com>
2023-05-04 19:48:31 +0300

    Fix knative uninstall script

--- stacks/knative/deploy.sh
+++ stacks/knative/deploy.sh
@@ -2,22 +2,20 @@
 
 set -e
 
-OPERATOR_VERSION="1.5.1"
+OPERATOR_VERSION="1.10.0"
 
-if [ -z "${MP_KUBERNETES}" ]; then
-  # use local version of knative serving and eventing manifests
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  KNATIVE_SERVING_COMPONENT="$ROOT_DIR/stacks/knative/assets/manifests/knative-serving.yaml"
-  KNATIVE_EVENTING_COMPONENT="$ROOT_DIR/stacks/knative/assets/manifests/knative-eventing.yaml"
-else
-  # use github hosted master version of knative serving and eventing manifests
-  KNATIVE_SERVING_COMPONENT="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/knative/assets/manifests/knative-serving.yaml"
-  KNATIVE_EVENTING_COMPONENT="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/knative/assets/manifests/knative-eventing.yaml"
-fi
+kubectl apply -f "https://github.com/knative/serving/releases/download/knative-v${OPERATOR_VERSION}/serving-crds.yaml"
+kubectl apply -f "https://github.com/knative/serving/releases/download/knative-v${OPERATOR_VERSION}/serving-core.yaml"
+kubectl apply -f "https://github.com/knative/net-kourier/releases/download/knative-v${OPERATOR_VERSION}/kourier.yaml"
+kubectl patch configmap/config-network \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"ingress.class":"kourier.ingress.networking.knative.dev"}}'
+
+kubectl apply -f "https://github.com/knative/eventing/releases/download/knative-v${OPERATOR_VERSION}/eventing-crds.yaml"
+kubectl apply -f "https://github.com/knative/eventing/releases/download/knative-v${OPERATOR_VERSION}/eventing-core.yaml"
+
+kubectl apply -f "https://github.com/knative/eventing/releases/download/knative-v${OPERATOR_VERSION}/mt-channel-broker.yaml"
 
 # Deploy Knative Operator
 kubectl apply -f "https://github.com/knative/operator/releases/download/knative-v${OPERATOR_VERSION}/operator.yaml" --wait
-
-# Deploy Knative Serving and Eventing components
-kubectl apply -f "$KNATIVE_SERVING_COMPONENT"
-kubectl apply -f "$KNATIVE_EVENTING_COMPONENT"
