7c7664da3db28a5fff37b20dc6396f19ac228425
Valera Bronshtein <105710429+valeraBr@users.noreply.github.com>
2023-06-12 20:52:51 +0300

    Memphis.dev release 1.0.3  (#461)
    * Update deploy.sh
    
    * Update upgrade.sh
    
    * Update upgrade.sh
    
    * Update values.yml

--- stacks/memphis/upgrade.sh
+++ stacks/memphis/upgrade.sh
@@ -14,6 +14,11 @@ helm repo update > /dev/null
 STACK="memphis"
 CHART="memphis/memphis"
 NAMESPACE="memphis"
+CT=$(kubectl get secret --namespace "memphis" memphis-creds -o jsonpath="{.data.CONNECTION_TOKEN}" | base64 -d)
+ROOT_PASSWORD=$(kubectl get secret --namespace "memphis" memphis-creds -o jsonpath="{.data.ROOT_PASSWORD}" | base64 -d)
+PASSWORD=$(kubectl get secret --namespace "memphis" memphis-metadata -o jsonpath="{.data.password}" | base64 -d)
+REPMGR_PASSWORD=$(kubectl get secret --namespace "memphis" memphis-metadata -o jsonpath="{.data.repmgr-password}" | base64 -d)
+ADMIN_PASSWORD=$(kubectl get secret --namespace "memphis" memphis-metadata-coordinator -o jsonpath="{.data.admin-password}" | base64 -d)
 
 if [ -z "${MP_KUBERNETES}" ]; then
     # use local version of values.yml
@@ -21,9 +26,14 @@ if [ -z "${MP_KUBERNETES}" ]; then
     values="$ROOT_DIR/stacks/memphis/values.yml"
 else
     # use github hosted master version of values.yml
-    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/memphis/values.yml"
+    values="https://raw.githubusercontent.com/memphisdev/memphis-k8s/gh-pages/memphis/values.yaml"
 fi
 
 helm upgrade "$STACK" "$CHART" \
 --namespace "$NAMESPACE" \
 --values "$values" \
+--set metadata.postgresql.password=$PASSWORD,\
+metadata.postgresql.repmgrPassword=$REPMGR_PASSWORD,\
+metadata.pgpool.adminPassword=$ADMIN_PASSWORD,\
+connectionToken=$CT,\
+rootPwd=$ROOT_PASSWORD
