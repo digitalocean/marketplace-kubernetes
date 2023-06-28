b21dec3b2397907a84260b96e325abeba29a3c8d
Gutium Alexei <v-agutium@digitalocean.com>
2023-06-28 18:15:25 +0300

    Testing without creationTimestamp

--- stacks/gopaddle-lite/deploy.sh
+++ stacks/gopaddle-lite/deploy.sh
@@ -16,18 +16,18 @@ CHART="gopaddle/gp-installer"
 CHART_VERSION="4.2.6"
 NAMESPACE="gp-lite-4-2"
 
-if [ -z "${MP_KUBERNETES}" ]; then
+#if [ -z "${MP_KUBERNETES}" ]; then
   # use local version of values.yml
-  ROOT_DIR=$(git rev-parse --show-toplevel)
-  values="$ROOT_DIR/stacks/gopaddle-lite/values.yml"
-  clusterrole="$ROOT_DIR/stacks/gopaddle-lite/clusterrole.yaml"
-  clusterrolebinding="$ROOT_DIR/stacks/gopaddle-lite/clusterrolebinding.yaml"
-else
+#  ROOT_DIR=$(git rev-parse --show-toplevel)
+#  values="$ROOT_DIR/stacks/gopaddle-lite/values.yml"
+#  clusterrole="$ROOT_DIR/stacks/gopaddle-lite/clusterrole.yaml"
+#  clusterrolebinding="$ROOT_DIR/stacks/gopaddle-lite/clusterrolebinding.yaml"
+#else
   # use github hosted master version of values.yml
   values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/values.yml"
   clusterrole="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/clusterrole.yaml"
   clusterrolebinding="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/clusterrolebinding.yaml"
-fi
+#fi
 
 kubectl apply -f "$clusterrole"
 kubectl apply -f "$clusterrolebinding"
