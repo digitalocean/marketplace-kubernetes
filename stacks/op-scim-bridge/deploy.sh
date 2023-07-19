fe0d709e692067e8d87fa323445cb2ff1532b954
Graham <grellyd@users.noreply.github.com>
2023-07-19 09:18:37 -0600

    Update stacks/op-scim-bridge/deploy.sh with GitHub hosted master version
    Co-authored-by: mbeyt <57464615+mbeyt@users.noreply.github.com>

--- stacks/op-scim-bridge/deploy.sh
+++ stacks/op-scim-bridge/deploy.sh
@@ -15,8 +15,14 @@ RELEASE="op-scim-bridge"
 NAMESPACE="op-scim-bridge"
 STORAGE_CLASS="do-block-storage"
 
-ROOT_DIR=$(git rev-parse --show-toplevel)
-VALUES_FILE="$ROOT_DIR/stacks/op-scim-bridge/values.yml"
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  VALUES_FILE="$ROOT_DIR/stacks/op-scim-bridge/values.yml"
+else
+  # use github hosted master version of values.yml
+  VALUES_FILE="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/op-scim-bridge/values.yml"
+fi
 
 helm upgrade "$RELEASE" "$REPO_NAME/$CHART_NAME" \
   --atomic \
