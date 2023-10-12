43f84856b90ab7d45208d71b4fc7d23a7face28a
Chi Su <chi@webb.ai>
2023-10-12 16:48:12 -0700

    generate webb.ai stack

---
+++ stacks/webb.ai/upgrade.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add webb.ai https://webb-ai.github.io/helm-charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="webb.ai"
+CHART="webb.ai/webbai-agent"
+NAMESPACE="webbai"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/webb.ai/values.yml"
+else
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/webb.ai/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+--namespace "$NAMESPACE" \
+--values "$values" \
