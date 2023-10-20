6c4517d4183f56757269a68e1cd222d62cf7c5a2
Chi Su <sunstick@gmail.com>
2023-10-20 12:46:39 -0700

    webb.ai first pr (#484)
    * generate webb.ai stack
    
    * change stack name
    
    * add images
    
    * update version
    
    * update upgrade.sh
    
    * formatting
    
    ---------
    
    Co-authored-by: Chi Su <chi@webb.ai>

---
+++ stacks/webb.ai/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add webb.ai https://webb-ai.github.io/helm-charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="webbai-agent"
+CHART="webb.ai/webbai-agent"
+CHART_VERSION="0.1.6"
+NAMESPACE="webbai"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/webb.ai/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/webb.ai/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --timeout 8m0s \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
