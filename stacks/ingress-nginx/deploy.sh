751ba8bc89856c1373a75a3232273c1d9d42bfb9
Gutium Alexei <v-agutium@digitalocean.com>
2023-10-24 18:02:06 +0300

    Update nginx controller to 1.9.3

--- stacks/ingress-nginx/deploy.sh
+++ stacks/ingress-nginx/deploy.sh
@@ -13,7 +13,7 @@ helm repo update > /dev/null
 ################################################################################
 STACK="ingress-nginx"
 CHART="ingress-nginx/ingress-nginx"
-CHART_VERSION="4.5.2"
+CHART_VERSION="4.8.2"
 NAMESPACE="ingress-nginx"
 
 if [ -z "${MP_KUBERNETES}" ]; then
