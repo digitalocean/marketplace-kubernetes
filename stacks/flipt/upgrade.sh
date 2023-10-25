e12f7909a4e5ae37dc3303142f993aeaa868d5f8
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-10-25 10:39:38 -0400

    feat: add flipt k8s stack (#486)

---
+++ stacks/flipt/upgrade.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add flipt https://helm.flipt.io
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="flipt"
+CHART="flipt/flipt"
+NAMESPACE="flipt"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/flipt/values.yml"
+else
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/flipt/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+--namespace "$NAMESPACE" \
+--values "$values" \
