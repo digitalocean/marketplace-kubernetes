fd55a780252437d17beaa44c812e64b6f1d73648
Oz <abdirahman.osmanthus@gmail.com>
2023-11-30 15:34:11 -0600

    add ngrok-ingress-controller to digital ocean (#492)

---
+++ stacks/ngrok-ingress-controller/upgrade.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add ngrok https://ngrok.github.io/kubernetes-ingress-controller
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="ngrok-ingress-controller"
+CHART="ngrok/kubernetes-ingress-controller"
+NAMESPACE="ngrok-ingress-controller"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/ngrok-ingress-controller/values.yml"
+else
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/ngrok-ingress-controller/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+--namespace "$NAMESPACE" \
+--values "$values" \
