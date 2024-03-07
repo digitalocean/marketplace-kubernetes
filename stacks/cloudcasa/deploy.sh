8f77398eb10abcc3fd3ae27dbef95f3a4674d3f1
Bob A <bobadair@users.noreply.github.com>
2024-03-06 22:00:45 -0500

    Update CloudCasa Helm chart to v3.4.2

--- stacks/cloudcasa/deploy.sh
+++ stacks/cloudcasa/deploy.sh
@@ -13,7 +13,7 @@ helm repo update > /dev/null
 ################################################################################
 STACK="cloudcasa"
 CHART="cloudcasa-helmchart/cloudcasa"
-CHART_VERSION="3.4.0"
+CHART_VERSION="3.4.2"
 NAMESPACE="default"
 
 if [ -z "${MP_KUBERNETES}" ]; then
