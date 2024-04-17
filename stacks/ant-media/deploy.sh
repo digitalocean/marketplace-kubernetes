1ebd6885dc7f2305c543303ad006e8bc7f06e722
Murat Ugur Eminoglu <murat@murat.ws>
2024-04-17 23:07:55 +0300

    Update deploy.sh

--- stacks/ant-media/deploy.sh
+++ stacks/ant-media/deploy.sh
@@ -13,7 +13,7 @@ helm repo update > /dev/null
 ################################################################################
 STACK="antmedia"
 CHART="antmedia/antmedia"
-CHART_VERSION="2.8.2"
+CHART_VERSION="2.9.0"
 NAMESPACE="antmedia"
 
 if [ -z "${MP_KUBERNETES}" ]; then
