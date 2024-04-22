31a818ed8f1cffcf9870dbbbdd6b2d757f04776e
John Dietz <john@kubefirst.com>
2024-04-22 09:42:26 -0400

    kubefirst upgrade to 2.4.6 in marketplace

--- stacks/kubefirst/deploy.sh
+++ stacks/kubefirst/deploy.sh
@@ -13,7 +13,7 @@ helm repo update > /dev/null
 ################################################################################
 STACK="kubefirst"
 CHART="kubefirst/kubefirst"
-CHART_VERSION="2.3.7"
+CHART_VERSION="2.4.6"
 NAMESPACE="kubefirst"
 
 if [ -z "${MP_KUBERNETES}" ]; then
