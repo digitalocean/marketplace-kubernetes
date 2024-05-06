50f1382c6e4866a65579fedd341422f8f37fe047
tyler.a <44843805+tylerasai@users.noreply.github.com>
2024-05-06 08:55:13 -0600

    Bump version 2.9.4 (#519)

--- stacks/op-scim-bridge/deploy.sh
+++ stacks/op-scim-bridge/deploy.sh
@@ -9,7 +9,7 @@ helm repo add "$REPO_NAME" "$REPO_URL"
 helm repo update > /dev/null
 
 CHART_NAME="op-scim-bridge"
-CHART_VERSION="2.11.2"
+CHART_VERSION="2.11.3"
 
 RELEASE="op-scim-bridge"
 NAMESPACE="op-scim-bridge"
