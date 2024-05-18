61e28f7c02b6086e402e9e6c94ea6d53d24ffdc6
Joao Gilberto Magalhaes <joao@byjg.com.br>
2024-05-18 18:26:30 -0500

    Minor adjusts.

--- stacks/easyhaproxy/uninstall.sh
+++ stacks/easyhaproxy/uninstall.sh
@@ -8,6 +8,13 @@ set -e
 STACK="easyhaproxy"
 NAMESPACE="easyhaproxy"
 
+# Check if there is any node with the label "easyhaproxy/node=master"
+master_node_exists=$(kubectl get nodes --selector=easyhaproxy/node=master -o jsonpath='{.items[*].metadata.name}')
+
+# If there is a node with the label, then remove the label
+if [ -n "$master_node_exists" ]; then
+    kubectl label nodes "$master_node_exists" "easyhaproxy/node-"
+fi
 
 helm uninstall "$STACK" \
   --namespace "$NAMESPACE"
