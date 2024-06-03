092ce20830b3234cf7a842cf9fc3b2d620b693a7
Hemal1P <104948125+Hemal1P@users.noreply.github.com>
2024-06-03 08:34:05 -0600

    bump to v2.9.5 (#522)

--- stacks/op-scim-bridge/deploy.sh
+++ stacks/op-scim-bridge/deploy.sh
@@ -9,7 +9,7 @@ helm repo add "$REPO_NAME" "$REPO_URL"
 helm repo update > /dev/null
 
 CHART_NAME="op-scim-bridge"
-CHART_VERSION="2.11.3"
+CHART_VERSION="2.11.4"
 
 RELEASE="op-scim-bridge"
 NAMESPACE="op-scim-bridge"
