03873efb49133af8d31b25d885cbfd833d287362
Gayathri R <gayathri.r@gopaddle.io>
2024-06-20 19:08:31 +0530

    gopaddle: Low-Code Kubernetes IDE with AI Co-pilot

--- stacks/gopaddle-lite/deploy.sh
+++ stacks/gopaddle-lite/deploy.sh
@@ -11,36 +11,36 @@ helm repo update > /dev/null
 ################################################################################
 # chart
 ################################################################################
-STACK="gp-lite-4-2"
-CHART="gopaddle/gp-installer"
-CHART_VERSION="4.2.6"
-NAMESPACE="gp-lite-4-2"
+STACK="gopaddle"
+CHART="gopaddle/gopaddle"
+CHART_VERSION="5.0"
+NAMESPACE="gopaddle"
 
 if [ -z "${MP_KUBERNETES}" ]; then
   # use local version of values.yml
   ROOT_DIR=$(git rev-parse --show-toplevel)
   values="$ROOT_DIR/stacks/gopaddle-lite/values.yml"
-  clusterrole="$ROOT_DIR/stacks/gopaddle-lite/clusterrole.yaml"
-  clusterrolebinding="$ROOT_DIR/stacks/gopaddle-lite/clusterrolebinding.yaml"
+  # clusterrole="$ROOT_DIR/stacks/gopaddle-lite/clusterrole.yaml"
+  # clusterrolebinding="$ROOT_DIR/stacks/gopaddle-lite/clusterrolebinding.yaml"
 else
   # use github hosted master version of values.yml
   values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/values.yml"
-  clusterrole="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/clusterrole.yaml"
-  clusterrolebinding="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/clusterrolebinding.yaml"
+  # clusterrole="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/clusterrole.yaml"
+  # clusterrolebinding="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/gopaddle-lite/clusterrolebinding.yaml"
 fi
 
-kubectl apply -f "$clusterrole"
-kubectl apply -f "$clusterrolebinding"
+# kubectl apply -f "$clusterrole"
+# kubectl apply -f "$clusterrolebinding"
 
 # Get the first node's external IP, if it exists
-FIRST_NODE_EXT_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="ExternalIP")].address}' 2>/dev/null)
+# FIRST_NODE_EXT_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="ExternalIP")].address}' 2>/dev/null)
 
-# If there's no external IP, get the internal IP
-if [ -z "$FIRST_NODE_EXT_IP" ]; then
-        FIRST_NODE_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="InternalIP")].address}' 2>/dev/null)
-else
-        FIRST_NODE_IP="$FIRST_NODE_EXT_IP"
-fi
+# # If there's no external IP, get the internal IP
+# if [ -z "$FIRST_NODE_EXT_IP" ]; then
+#         FIRST_NODE_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="InternalIP")].address}' 2>/dev/null)
+# else
+#         FIRST_NODE_IP="$FIRST_NODE_EXT_IP"
+# fi
 
 helm upgrade "$STACK" "$CHART" \
   --atomic \
@@ -49,5 +49,4 @@ helm upgrade "$STACK" "$CHART" \
   --timeout 15m0s \
   --namespace "$NAMESPACE" \
   --values "$values" \
-  --set global.cluster.nodeIP="$FIRST_NODE_IP" \
   --version "$CHART_VERSION"
--- stacks/gopaddle-lite/upgrade.sh
+++ stacks/gopaddle-lite/upgrade.sh
@@ -11,10 +11,10 @@ helm repo update > /dev/null
 ################################################################################
 # chart
 ################################################################################
-STACK="gp-lite-4-2"
-CHART="gopaddle/gp-installer"
-NAMESPACE="gp-lite-4-2"
-CHART_VERSION="4.2.6"
+STACK="gopaddle"
+CHART="gopaddle/gopaddle"
+NAMESPACE="gopaddle"
+CHART_VERSION="5.0"
 
 
 if [ -z "${MP_KUBERNETES}" ]; then
@@ -27,17 +27,16 @@ else
 fi
 
 # Get the first node's external IP, if it exists
-FIRST_NODE_EXT_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="ExternalIP")].address}' 2>/dev/null)
+# FIRST_NODE_EXT_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="ExternalIP")].address}' 2>/dev/null)
 
-# If there's no external IP, get the internal IP
-if [ -z "$FIRST_NODE_EXT_IP" ]; then
-        FIRST_NODE_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="InternalIP")].address}' 2>/dev/null)
-else
-        FIRST_NODE_IP="$FIRST_NODE_EXT_IP"
-fi
+# # If there's no external IP, get the internal IP
+# if [ -z "$FIRST_NODE_EXT_IP" ]; then
+#         FIRST_NODE_IP=$(kubectl get nodes -o jsonpath='{$.items[0].status.addresses[?(@.type=="InternalIP")].address}' 2>/dev/null)
+# else
+#         FIRST_NODE_IP="$FIRST_NODE_EXT_IP"
+# fi
 
 helm upgrade "$STACK" "$CHART" \
   --namespace "$NAMESPACE" \
   --values "$values" \
-  --set global.cluster.nodeIP="$FIRST_NODE_IP" \
   --version "$CHART_VERSION"
