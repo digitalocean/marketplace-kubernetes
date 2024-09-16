a7b711ba9fb6fe746dd78d325ec458bd968efe9c
Jack Pearce <16779171+jkpe@users.noreply.github.com>
2024-09-16 11:07:50 +0100

    chore: Update NGINX Ingress Controller chart version to 4.11.2

--- stacks/ingress-nginx/deploy.sh
+++ stacks/ingress-nginx/deploy.sh
@@ -13,7 +13,7 @@ helm repo update > /dev/null
 ################################################################################
 STACK="ingress-nginx"
 CHART="ingress-nginx/ingress-nginx"
-CHART_VERSION="4.9.0"
+CHART_VERSION="4.11.2"
 NAMESPACE="ingress-nginx"
 
 if [ -z "${MP_KUBERNETES}" ]; then
