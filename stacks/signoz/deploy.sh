8ea9010676267dd116c34d98044a0883afe77676
Nageshbansal <76246968+Nageshbansal@users.noreply.github.com>
2025-08-05 01:54:12 +0530

    feat: add signoz stack (#558)
    * Adds signoz stack
    
    * Adds values.yml for signoz stack
    
    * Use latest tag for signoz images
    
    * Remove rebudant files

---
+++ stacks/signoz/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add signoz https://charts.signoz.io
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="signoz"
+CHART="signoz/signoz"
+CHART_VERSION="0.86.0"
+NAMESPACE="signoz"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/signoz/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/signoz/charts/charts/signoz/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --atomic \
+  --create-namespace \
+  --install \
+  --timeout 8m0s \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
