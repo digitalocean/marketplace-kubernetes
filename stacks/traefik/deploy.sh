29db4c8fe5862710b91e47bbd48c9431c0591ed9
Jack Pearce <16779171+jkpe@users.noreply.github.com>
2025-08-07 14:24:12 +0100

    Add Traefik stack (#560)

---
+++ stacks/traefik/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add traefik https://traefik.github.io/charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="traefik"
+CHART="traefik/traefik"
+CHART_VERSION="37.0.0"
+NAMESPACE="traefik"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+  # use local version of values.yml
+  ROOT_DIR=$(git rev-parse --show-toplevel)
+  values="$ROOT_DIR/stacks/traefik/values.yml"
+else
+  # use github hosted master version of values.yml
+  values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/traefik/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+  --create-namespace \
+  --install \
+  --namespace "$NAMESPACE" \
+  --values "$values" \
+  --version "$CHART_VERSION"
---
+++ stacks/traefik/upgrade.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+################################################################################
+# repo
+################################################################################
+helm repo add traefik https://traefik.github.io/charts
+helm repo update > /dev/null
+
+################################################################################
+# chart
+################################################################################
+STACK="traefik"
+CHART="traefik/traefik"
+NAMESPACE="traefik"
+
+if [ -z "${MP_KUBERNETES}" ]; then
+    # use local version of values.yml
+    ROOT_DIR=$(git rev-parse --show-toplevel)
+    values="$ROOT_DIR/stacks/traefik/values.yml"
+else
+    # use github hosted master version of values.yml
+    values="https://raw.githubusercontent.com/digitalocean/marketplace-kubernetes/master/stacks/traefik/values.yml"
+fi
+
+helm upgrade "$STACK" "$CHART" \
+--namespace "$NAMESPACE" \
+--values "$values"
