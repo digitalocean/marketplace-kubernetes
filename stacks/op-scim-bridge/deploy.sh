bb14bac3efbdc34c1741def7e439b07178a43d1e
Graham <grellyd@users.noreply.github.com>
2025-10-03 14:04:51 -0500

    Update the 1Password SCIM bridge helm chart to v2.11.10 (#568)

--- stacks/op-scim-bridge/deploy.sh
+++ stacks/op-scim-bridge/deploy.sh
@@ -9,7 +9,7 @@ helm repo add "$REPO_NAME" "$REPO_URL"
 helm repo update > /dev/null
 
 CHART_NAME="op-scim-bridge"
-CHART_VERSION="2.11.9"
+CHART_VERSION="2.11.10"
 
 RELEASE="op-scim-bridge"
 NAMESPACE="op-scim-bridge"
