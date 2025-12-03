b8d8e2f78c44ddc8704798feae5df809b28c7ac4
mbeyt <mbeyt@digitalocean.com>
2025-12-03 16:45:02 -0500

    use npm/ncc to build compiled js

--- .github/actions/auto-comment-bot/dist/reaction.d.ts
+++ .github/actions/auto-comment-bot/dist/reaction.d.ts
@@ -1,5 +1,61 @@
 import * as github from '@actions/github';
 export declare namespace Reaction {
     function add(octokit: ReturnType<typeof github.getOctokit>, comment_id: number, // tslint:disable-line
-    reactions: string | string[], owner?: string, repo?: string): Promise<(import("@octokit/types").OctokitResponse<import("@octokit/types").ReactionsCreateForIssueCommentResponseData> | undefined)[] | undefined>;
+    reactions: string | string[], owner?: string, repo?: string): Promise<(import("@octokit/types").OctokitResponse<{
+        id: number;
+        node_id: string;
+        user: {
+            name?: string | null | undefined;
+            email?: string | null | undefined;
+            login: string;
+            id: number;
+            node_id: string;
+            avatar_url: string;
+            gravatar_id: string | null;
+            url: string;
+            html_url: string;
+            followers_url: string;
+            following_url: string;
+            gists_url: string;
+            starred_url: string;
+            subscriptions_url: string;
+            organizations_url: string;
+            repos_url: string;
+            events_url: string;
+            received_events_url: string;
+            type: string;
+            site_admin: boolean;
+            starred_at?: string | undefined;
+        } | null;
+        content: "+1" | "-1" | "laugh" | "confused" | "heart" | "hooray" | "rocket" | "eyes";
+        created_at: string;
+    }, 200> | import("@octokit/types").OctokitResponse<{
+        id: number;
+        node_id: string;
+        user: {
+            name?: string | null | undefined;
+            email?: string | null | undefined;
+            login: string;
+            id: number;
+            node_id: string;
+            avatar_url: string;
+            gravatar_id: string | null;
+            url: string;
+            html_url: string;
+            followers_url: string;
+            following_url: string;
+            gists_url: string;
+            starred_url: string;
+            subscriptions_url: string;
+            organizations_url: string;
+            repos_url: string;
+            events_url: string;
+            received_events_url: string;
+            type: string;
+            site_admin: boolean;
+            starred_at?: string | undefined;
+        } | null;
+        content: "+1" | "-1" | "laugh" | "confused" | "heart" | "hooray" | "rocket" | "eyes";
+        created_at: string;
+    }, 201> | undefined)[] | undefined>;
 }
